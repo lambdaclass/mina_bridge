5fc2edb41a8dca2866c1ec6fab2c45c2c252d0c2
gabrielbosio <gabrielbosio95@gmail.com>
2023-09-13 12:22:56 -0300

    Remove scalar usage

--- evm_bridge/src/Verifier.ts
+++ evm_bridge/src/Verifier.ts
@@ -3,16 +3,16 @@ import { SRS } from './SRS.js';
 
 let { g, h } = SRS.createFromJSON();
 
-const z1 = Scalar.from(11164968105101346800601138014004522679567438778713307333394634182972316008497n);
-const sg = new Group({ x: 15278981249295424160498722087900196657161522262984020265396573104489152116709n, y: 4850314952377183854204710264495149520289203330774235803994549591326220970677n });
-const expected = new Group({ x: 1147286326535894773798565427772816968384738437752833585359811205574455608001n, y: 14452503934711747476944141550942840832396802557030426136987723106749969725488n });
+const z1 = 25941210464058333615640277621650698115212711614201964880539255585340602439563n;
+const sg = new Group({ x: 13083261687194515069435447694875777772013102442934202017859940102123715707411n, y: 3304437928237879390637239761250612103836290340416580684668461108572943688529n });
+const expected = new Group({ x: 25898394755259204300293837348855282619150839292818703595205424265266533863003n, y: 27621775263417330268828823966510023901196203314880122974412190362158599366097n });
 
-const randBase = Scalar.from(1);
-const negRandBase = randBase.neg();
-const sgRandBase = Scalar.from(1);
+const randBase = 1n;
+const negRandBase = Scalar.from(randBase).neg().toBigInt();
+const sgRandBase = 1n;
 
 export class Verifier extends Circuit {
-  // @circuitMain
+  @circuitMain
   static main() {
     let nonzero_length = g.length;
     let max_rounds = Math.ceil(Math.log2(nonzero_length));
@@ -20,34 +20,25 @@ export class Verifier extends Circuit {
     let padding = padded_length - nonzero_length;
 
     let points = [h];
-    console.log("points len:", points.length);
     points.concat(g);
-    console.log("points len:", points.length);
     points.concat(Array(padding).fill(Group.zero));
-    console.log("points len:", points.length);
 
-    let scalars = Array(padded_length + 1).fill(Scalar.from(0));
-    console.log("scalars len:", scalars.length);
+    let scalars = Array(padded_length + 1).fill(0n);
 
     points.push(sg);
-    console.log("points len:", points.length);
-    scalars.push(negRandBase.mul(z1).sub(sgRandBase));
-    console.log("scalars len:", scalars.length);
+    scalars.push(negRandBase * z1 - sgRandBase);
 
-    let s = Array(g.length).fill(Scalar.from(1));
-    console.log("s len:", s.length);
+    let s = Array(g.length).fill(1n);
     for (let i = 0; i < s.length; i++) {
-      let term = sgRandBase.mul(s[i]);
-      scalars[i + 1] = scalars[i + 1].add(term);
-      console.log("i:", i, "term:", term.toBigInt(), "s:", scalars[i + 1].toBigInt());
+      let term = sgRandBase * s[i];
+      scalars[i + 1] += term;
     }
 
-    console.log("msm!");
     Verifier.msm(points, scalars).assertEquals(expected);
   }
 
   // Naive algorithm
-  static msm(points: Group[], scalars: Scalar[]) {
+  static msm(points: Group[], scalars: bigint[]) {
     let result = Group.zero;
 
     for (let i = 0; i < points.length; i++) {
--- evm_bridge/src/main.ts
+++ evm_bridge/src/main.ts
@@ -5,13 +5,9 @@ console.log('SnarkyJS loaded');
 
 // ----------------------------------------------------
 
-console.log("Running circuit...");
-Verifier.main();
-console.log("Done!");
-
-// console.log("Generating constraint system");
-// let cs = Provable.constraintSystem(Verifier.main);
-// console.log("Constraint system:", cs);
+console.log("Generating constraint system");
+let cs = Provable.constraintSystem(Verifier.main);
+console.log("Constraint system:", cs);
 
 // ----------------------------------------------------
 console.log('Shutting down');
