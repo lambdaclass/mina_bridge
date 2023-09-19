9893d1d5b4d9266a9e71805cde38fb7cbe4ce1d8
gabrielbosio <gabrielbosio95@gmail.com>
2023-09-19 12:46:51 -0300

    Fix SRS test

--- evm_bridge/src/SRS.test.ts
+++ evm_bridge/src/SRS.test.ts
@@ -5,12 +5,12 @@ test("Deserialize SRS and check fields", () => {
     let srs = SRS.createFromJSON();
     let first_g = srs.g[0];
 
-    let expected_g_x = Field(0xf860fc1253c58c46f6c5afc51f0f66e7523ed4f8aa851370a9d55f8826441c12n);
-    let expected_g_y = Field(0x708b02abd83f6dd966f7dd807761af08b14c4e32ebddc51835ea4712c039b421n);
+    let expected_g_x = Field(24533576165769248459550833334830854594262873459712423377895708212271843679280n);
+    let expected_g_y = Field(1491943283321085992458304042389285332496706344738505795532548822057073739620n);
     expect(first_g.x).toEqual(expected_g_x);
     expect(first_g.y).toEqual(expected_g_y);
-    let expected_h_x = Field(0x012226265bceb2e5a8c78be27579a29c3636787563f2b4aa99c9016338602009n);
-    let expected_h_y = Field(0x26bebc5e9c2420ce85ee05589b9a206a60f26561be419d9f506ae63ad8c44f31n);
+    let expected_h_x = Field(15427374333697483577096356340297985232933727912694971579453397496858943128065n);
+    let expected_h_y = Field(2509910240642018366461735648111399592717548684137438645981418079872989533888n);
     expect(srs.h.x).toEqual(expected_h_x);
     expect(srs.h.y).toEqual(expected_h_y);
 });
