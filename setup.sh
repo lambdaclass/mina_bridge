aa6a130ee7cd499487bca09c613e9fcdc7c8e665
gabrielbosio <gabrielbosio95@gmail.com>
2023-09-28 22:02:08 +0200

    Add install steps before compiling bindings

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 git clone https://github.com/lambdaclass/mina_monorepo.git
 cd mina_monorepo
 git checkout remove_automation_dir
+opam install dune
+opam switch import opam.export
 cd src/lib/snarkyjs
 npm run bindings
 npm run build
