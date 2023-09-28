b9ace7941a444d1e00999aaef339989f0010d90a
gabrielbosio <gabrielbosio95@gmail.com>
2023-09-28 21:30:17 +0200

    Change monorepo branch to remove automation one

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/lambdaclass/mina_monorepo.git
 cd mina_monorepo
-git checkout develop
+git checkout remove_automation_dir
 cd src/lib/snarkyjs
 npm run bindings
 npm run build
