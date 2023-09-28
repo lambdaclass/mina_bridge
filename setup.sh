40878167edd915b2258bf0b0b412848ef3a1ac34
gabrielbosio <gabrielbosio95@gmail.com>
2023-09-28 22:17:25 +0200

    Add 'yes' flag to opam cmds

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 git clone https://github.com/lambdaclass/mina_monorepo.git
 cd mina_monorepo
 git checkout remove_automation_dir
-opam install dune
-opam switch import opam.export
+opam install dune -y
+opam switch import opam.export -y
 cd src/lib/snarkyjs
 npm run bindings
 npm run build
