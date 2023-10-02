97ae3c64e2c060e71fb15753d6df536865885e2a
gabrielbosio <gabrielbosio95@gmail.com>
2023-10-02 16:38:16 +0200

    Add opam env cmd

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ git clone https://github.com/lambdaclass/mina_monorepo.git
 cd mina_monorepo
 git checkout develop
 opam install dune -y
+eval $(opam env)
 opam switch import opam.export -y
 cd src/lib/snarkyjs
 npm run bindings
