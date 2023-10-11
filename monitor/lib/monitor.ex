7a056c632d581d65f073f6a141b0eb4674bec105
Pablo Deymonnaz <deymonnaz@gmail.com>
2023-10-11 17:05:52 -0300

    Skeleton of Elixir monitor

---
+++ monitor/lib/monitor.ex
@@ -0,0 +1,18 @@
+defmodule Monitor do
+  @moduledoc """
+  Documentation for `Monitor`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Monitor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
