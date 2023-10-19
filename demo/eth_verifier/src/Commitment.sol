48bb393a47735308324f107a963cd0fa72750730
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-10-19 15:55:22 -0300

    WIP step 2

--- demo/eth_verifier/src/Commitment.sol
+++ demo/eth_verifier/src/Commitment.sol
@@ -3,11 +3,15 @@ pragma solidity >=0.4.16 <0.9.0;
 
 import {BN254} from "./BN254.sol";
 
-library Commitment {
-    struct PolyComm {
-        BN254.G1[] unshifted;
-        //BN254G1 shifted;
-        // WARN: The previous field is optional but in Solidity we can't have that.
-        // for our test circuit (circuit_gen/) it's not necessary
-    }
+struct URS {
+    BN254.G1[] g;
+    BN254.G1 h;
+    mapping(uint => PolyComm[]) lagrange_bases;
+}
+
+struct PolyComm {
+    BN254.G1[] unshifted;
+    //BN254G1 shifted;
+    // WARN: The previous field is optional but in Solidity we can't have that.
+    // for our test circuit (circuit_gen/) it's not necessary
 }
