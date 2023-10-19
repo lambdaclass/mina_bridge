67ff0cdfe001dc51d9f23c9059300dd64eab1a7e
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-10-19 14:36:29 -0300

    Removed inv

--- demo/eth_verifier/src/Fields.sol
+++ demo/eth_verifier/src/Fields.sol
@@ -1,33 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.16 <0.9.0;
 
-/// @notice Extended euclidean algorithm. Returns [gcd, Bezout_a, Bezout_b]
-/// @notice so gcd = a*Bezout_a + b*Bezout_b.
-/// @notice source: https://www.extendedeuclideanalgorithm.com/code
-function xgcd(
-    uint256 a,
-    uint256 b,
-    uint256 s1,
-    uint256 s2,
-    uint256 t1,
-    uint256 t2
-) pure returns (uint256, uint256, uint256) {
-    if (b == 0) {
-        return (a, 1, 0);
-    }
-
-    uint256 q = a / b;
-    uint256 r = a - q * b;
-    uint256 s3 = s1 - q * s2;
-    uint256 t3 = t1 - q * t2;
-
-    if (r == 0) {
-        return (b, s2, t2);
-    } else {
-        return xgcd(b, r, s2, s3, t2, t3);
-    }
-}
-
 /// @notice Implements 256 bit modular arithmetic over the base field of bn254.
 library Base {
     type FE is uint256;
@@ -55,20 +28,9 @@ library Base {
         }
     }
 
-    function inv(FE self) public pure returns (FE) {
-        (uint256 gcd, uint256 res, uint256 _x) = xgcd(
-            FE.unwrap(self),
-            MODULUS,
-            1,
-            0,
-            0,
-            1
-        );
-        if (gcd != 1) {
-            // FIXME: error
-        }
-
-        return FE.wrap(res);
+    function inv(FE self) public view returns (FE) {
+        // TODO:
+        return FE.wrap(0);
     }
 }
 
@@ -99,19 +61,8 @@ library Scalar {
         }
     }
 
-    function inv(FE self) public pure returns (FE) {
-        (uint256 gcd, uint256 res, uint256 _x) = xgcd(
-            FE.unwrap(self),
-            MODULUS,
-            1,
-            0,
-            0,
-            1
-        );
-        if (gcd != 1) {
-            // FIXME: error
-        }
-
-        return FE.wrap(res);
+    function inv(FE self) public view returns (FE) {
+        // TODO:
+        return FE.wrap(0);
     }
 }
