15e3f0d2904b6a1144eff80b644b081c4c24d9a6
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-10-24 16:22:24 -0300

    Refactor, cleared some code

---
+++ demo/eth_verifier/src/msgpack/Deserialize.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+import "../Verifier.sol";
+import "../BN254.sol";
+
+library MsgPk {
+
+    function deserializeOpeningProof(
+        bytes calldata serialized_proof
+    ) public view returns (Kimchi.ProverProof memory proof) {
+        uint256 i = 0;
+        bytes1 firstbyte = serialized_proof[i];
+        // first byte is 0x92, indicating this is a map with 2 elements
+        require(firstbyte == 0x92, "first byte is not 0x92");
+
+        // read lenght of the data
+        i += 1;
+        require(serialized_proof[i] == 0xC4, "second byte is not 0xC4");
+
+        // next byte is the length of the data in one byte
+        i += 1;
+        require(serialized_proof[i] == 0x20, "size of element is not 32 bytes");
+
+        // read data
+        i += 1;
+        bytes32 data_quotient = abi.decode(serialized_proof[i:i+32], (bytes32));
+
+        proof.opening_proof_quotient = BN254.g1Deserialize(
+            data_quotient
+        );
+
+        // read blinding
+        i += 32;
+        // read length of the data
+        require(serialized_proof[i] == 0xC4, "second byte is not 0xC4");
+
+        // next byte is the length of the data in one byte
+        i += 1;
+        require(serialized_proof[i] == 0x20, "size of element is not 32 bytes");
+
+        // read data
+        i += 1;
+        uint256 data_blinding = abi.decode(serialized_proof[i:i+32], (uint256));
+
+        proof.opening_proof_blinding = data_blinding;
+        return proof;
+    }
+
+}
--- demo/eth_verifier/test/Verifier.t.sol
+++ demo/eth_verifier/test/Verifier.t.sol
@@ -6,6 +6,7 @@ import "forge-std/console.sol";
 import {KimchiVerifier, Kimchi} from "../src/Verifier.sol";
 import {BN254} from "../src/BN254.sol";
 import "../src/Fields.sol";
+import "../src/msgpack/Deserialize.sol";
 
 contract CounterTest is Test {
     KimchiVerifier public verifier;
@@ -30,19 +31,9 @@ contract CounterTest is Test {
     }
 
     function test_deserialize_opening_proof() public {
-        uint8[69] memory opening_proof_serialized = [
-            0x92, 0xc4, 0x20, 0x04, 0x08, 0x2c, 0x5f, 0xa2,
-            0x2d, 0x4d, 0x2b, 0xf7, 0x8f, 0x2a, 0xa7, 0x12,
-            0x69, 0x51, 0x09, 0x11, 0xc1, 0xb4, 0x14, 0xb8,
-            0xbe, 0xdf, 0xe4, 0x1a, 0xfb, 0x3c, 0x71, 0x47,
-            0xf9, 0x93, 0x25, 0xc4, 0x20, 0x17, 0xa3, 0xbf,
-            0xd7, 0x24, 0xd8, 0x8b, 0xf2, 0x3e, 0xd3, 0xd1,
-            0x31, 0x55, 0xcd, 0x09, 0xc0, 0xa4, 0xd1, 0xd1,
-            0xd5, 0x20, 0xb8, 0x69, 0x59, 0x9f, 0x00, 0x95,
-            0x88, 0x10, 0x10, 0x06, 0x21
-        ];
+        bytes memory opening_proof_serialized = hex"92c42004082c5fa22d4d2bf78f2aa71269510911c1b414b8bedfe41afb3c7147f99325c42017a3bfd724d88bf23ed3d13155cd09c0a4d1d1d520b869599f00958810100621";
 
-        Kimchi.ProverProof memory proof = Kimchi.deserializeOpeningProof(
+        Kimchi.ProverProof memory proof = MsgPk.deserializeOpeningProof(
             opening_proof_serialized
         );
 
