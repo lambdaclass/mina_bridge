d38340a27fd684361ae7b2e7076a54f2d24865d5
gabrielbosio <gabrielbosio95@gmail.com>
2023-10-31 23:44:06 +0100

    Change mina url from ssh to https

--- polling_service/run.sh
+++ polling_service/run.sh
@@ -7,7 +7,7 @@ curl -d '{"query": "{
 }"}' -H 'Content-Type: application/json' http://5.9.57.89:3085/graphql | sed -e 's@{\"data\":{\"bestChain":\[{\"protocolStateProof\":{\"base64\":\"@@' \
 	| sed -e 's@\(.*\)\"}}]}}@\1====@' | fold -w 4 | sed '$ d' | tr -d '\n' > proof.txt
 
-git clone git@github.com:lambdaclass/mina.git mina_1_4_0
+git clone https://github.com/lambdaclass/mina.git mina_1_4_0
 cd mina_1_4_0
 git remote add o1labs git@github.com:MinaProtocol/mina.git
 git checkout proof_to_json
