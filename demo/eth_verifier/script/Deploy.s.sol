5e1a53f21b2d6dffeaee32e8844735183c168e9e
d-roak <me+git@droak.sh>
2023-11-02 01:33:22 +0900

    pass private key in makefile

--- demo/eth_verifier/script/Deploy.s.sol
+++ demo/eth_verifier/script/Deploy.s.sol
@@ -8,7 +8,7 @@ import {KimchiVerifier} from "../src/Verifier.sol";
 
 contract Deploy is Script {
     function run() public {
-        uint256 deployerPrivateKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
         new KimchiVerifier();
