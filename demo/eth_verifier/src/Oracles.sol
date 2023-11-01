feb04b358b07f6a6743df68ff725eb9e1bec46d7
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-11-01 15:55:44 -0300

    Added to_field()

--- demo/eth_verifier/src/Oracles.sol
+++ demo/eth_verifier/src/Oracles.sol
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.16 <0.9.0;
 
-import "./primitives/Fields.sol";
+import "./Fields.sol";
 
 library Oracles {
-    using { to_field } for ScalarChallenge;
+    using { to_field_with_length, to_field } for ScalarChallenge;
+    using { Scalar.add, Scalar.mul, Scalar.neg, Scalar.double } for Scalar.FE;
+
+    uint64 internal constant CHALLENGE_LENGTH_IN_LIMBS = 2;
 
     function fiat_shamir() public view {
-        // We'll skip the use of a sponge and generate challenges from pseudo-random numbers
+        // WARN: We'll skip the use of a sponge and generate challenges from pseudo-random numbers
+        Scalar.FE endo_coeff = Scalar.from(0); // FIXME: not zero
 
         // Sample beta and gamma from the sponge
         Scalar.FE beta = challenge();
@@ -16,14 +20,19 @@ library Oracles {
         // Sample alpha prime
         ScalarChallenge memory alpha_chal = scalar_chal();
         // Derive alpha using the endomorphism
-        Scalar.FE alpha = alpha_chal.to_field();
+        Scalar.FE alpha = alpha_chal.to_field(endo_coeff);
+
+        // Sample alpha prime
+        ScalarChallenge memory zeta_chal = scalar_chal();
+        // Derive alpha using the endomorphism
+        Scalar.FE zeta = zeta_chal.to_field(endo_coeff);
     }
 
     /// @notice creates a challenge frm hashing the current block timestamp.
     /// @notice this function is only going to be used for the demo and never in
     /// @notice a serious environment. DO NOT use this in any other case.
     function challenge() internal view returns (Scalar.FE) {
-        Scalar.from(keccak256(abi.encode(block.timestamp)));
+        Scalar.from(uint256(keccak256(abi.encode(block.timestamp))));
     }
 
     /// @notice creates a `ScaharChallenge` using `challenge()`.
@@ -35,7 +44,72 @@ library Oracles {
         Scalar.FE chal;
     }
 
-    function to_field(ScalarChallenge memory sc) public pure returns (Scalar.FE) {
+    function to_field_with_length(
+        ScalarChallenge memory self,
+        uint length_in_bits,
+        Scalar.FE endo_coeff
+    ) internal pure returns (Scalar.FE) {
+        uint64[] memory r = get_limbs_64(Scalar.FE.unwrap(self.chal));
+        Scalar.FE a = Scalar.from(2);
+        Scalar.FE b = Scalar.from(2);
 
+        Scalar.FE one = Scalar.from(1);
+        Scalar.FE neg_one = one.neg();
+
+        for (uint64 i = 0; i < length_in_bits / 2; i++) {
+            a = a.double();
+            b = b.double();
+
+            uint64 r_2i = get_bit(r, 2 * i);
+            Scalar.FE s = r_2i == 0 ? neg_one : one;
+
+            if (get_bit(r, 2 * i + 1) == 0) {
+                b = b.add(s);
+            } else {
+                a = a.add(s);
+            }
+        }
+
+        return a.mul(endo_coeff).add(b);
+    }
+
+    function to_field(
+        ScalarChallenge memory self,
+        Scalar.FE endo_coeff
+    ) internal pure returns (Scalar.FE) {
+        uint64 length_in_bits = 64 * CHALLENGE_LENGTH_IN_LIMBS;
+        return self.to_field_with_length(length_in_bits, endo_coeff);
+    }
+
+    function get_bit(uint64[] memory limbs_lsb, uint64 i) internal pure returns (uint64) {
+        uint64 limb = i / 64;
+        uint64 j = i % 64;
+        return (limbs_lsb[limb] >> j) & 1;
+    }
+
+    /**
+    * Decomposes `n` into 64 bit limbs, less significant first
+    */
+     function get_limbs_64(uint256 n) internal pure returns (uint64[] memory limbs) {
+        uint len = 256 / 64;
+        uint128 mask_64 = (1 << 64) - 1;
+
+        limbs = new uint64[](len);
+        for (uint i = 0; i <= len; i++) {
+            limbs[i] = uint64(n & mask_64);
+            n >>= 64;
+        }
+
+        return limbs;
+    }
+
+    /**
+    * Recomposes 64-bit `limbs` into a bigint, less significant first
+    */
+    function from_limbs_64(uint64[] memory limbs) internal pure returns (uint256 n_rebuilt) {
+        n_rebuilt = 0;
+        for (uint i = 0; i < limbs.length; i++) {
+            n_rebuilt += limbs[i] << 64*i;
+        }
     }
 }
