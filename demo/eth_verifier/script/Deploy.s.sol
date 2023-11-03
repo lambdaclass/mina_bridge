a84d0e0880e975d8a25550cc98bd06ff9c884b3a
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-11-03 15:34:57 -0300

    Add deploy state script

--- demo/eth_verifier/script/Deploy.s.sol
+++ demo/eth_verifier/script/Deploy.s.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import "forge-std/console.sol";
 
 import {KimchiVerifier} from "../src/Verifier.sol";
 
@@ -11,6 +12,23 @@ contract Deploy is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         new KimchiVerifier();
+
+        vm.stopBroadcast();
+    }
+}
+
+contract DeployState is Script {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        KimchiVerifier verifier = new KimchiVerifier();
+
+        bytes memory state_data = hex"42";
+        verifier.store(state_data);
+
+        bytes memory retrieved_data = verifier.retrieve();
+        console.log(uint8(bytes1(retrieved_data)));
 
         vm.stopBroadcast();
     }
