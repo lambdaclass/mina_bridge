3201b687a839ea4fa331291c17143feb2e3274e3
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-11-06 13:22:51 -0300

    Fix comments

--- demo/state_serializer/src/main.rs
+++ demo/state_serializer/src/main.rs
@@ -1,12 +1,9 @@
 use std::env;
 
-// {"data":{"bestChain":[{"creator":"B62qoA5XwfEVnXbcrzphGH1TVuqxeJ5bhX7vTS3hcxpQFHnStG3MQk9","stateHashField":"24924803968503832121419666918600454255868948649936693214146949523817136720847",
-// "protocolState":{"consensusState":{"blockHeight":"303201"}}}]}}%
+// Example:
+// {"data":{"bestChain":[{"creator":"B62qoA5XwfEVnXbcrzphGH1TVuqxeJ5bhX7vTS3hcxpQFHnStG3MQk9","stateHashField":"24924803968503832121419666918600454255868948649936693214146949523817136720847","protocolState":{"consensusState":{"blockHeight":"303201"}}}]}}%
 
 fn main() {
-    // let query: serde_json::Value = serde_json::from_str(r#"
-    // {"data":{"bestChain":[{"creator":"B62qoA5XwfEVnXbcrzphGH1TVuqxeJ5bhX7vTS3hcxpQFHnStG3MQk9","stateHashField":"24924803968503832121419666918600454255868948649936693214146949523817136720847","protocolState":{"consensusState":{"blockHeight":"303201"}}}]}}
-    // "#).unwrap();
     let args: Vec<_> = env::args().collect();
     let query: serde_json::Value = serde_json::from_str(&args[1]).unwrap();
     let state = query
