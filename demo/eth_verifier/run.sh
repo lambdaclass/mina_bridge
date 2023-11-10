9fb6ee10d5f154619676c911a106950417f4ceaf
Gabriel Bosio <38794644+gabrielbosio@users.noreply.github.com>
2023-11-10 10:52:20 -0300

    Improve integration (#79)
    * Fix anvil issues in integration
    
    * Wait for anvil to setup

---
+++ demo/eth_verifier/run.sh
@@ -0,0 +1,12 @@
+(anvil &)
+
+echo "Waiting for local blockchain..."
+sleep 3
+
+PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
+forge script \
+    --via-ir \
+    --rpc-url http://127.0.0.1:8545 \
+    --broadcast \
+    script/Deploy.s.sol:Deploy
+pkill anvil || true
