12b638e278ad54455cde52238c66be5ccb1bfd48
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-11-15 16:33:33 -0300

    Finished base sponge

--- eth_verifier/lib/poseidon/Sponge.sol
+++ eth_verifier/lib/poseidon/Sponge.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.4.16 <0.9.0;
 
 import "../bn254/Fields.sol";
+import "./Permutation.sol";
 
 enum SpongeMode {
     Squeezing,
@@ -9,18 +10,19 @@ enum SpongeMode {
 }
 
 struct BaseSponge {
-    Base.FE[3] state; // rate = 2
+    Base.FE[3] state;
     SpongeMode mode;
     uint offset;
 }
 
 struct ScalarSponge {
-    Scalar.FE[3] state; // rate = 2
+    Scalar.FE[3] state;
     SpongeMode mode;
     uint offset;
 }
 
 library Sponge {
+    using {Base.mul, Base.add} for Base.FE;
     uint public constant RATE = 2;
 
     function new_base() public pure returns (BaseSponge memory) {
@@ -30,5 +32,29 @@ library Sponge {
                 SpongeMode.Absorbing,
                 0
             );
+    }
+
+    function absorb(BaseSponge memory self, Base.FE elem) public pure {
+        if (self.mode == SpongeMode.Squeezing) {
+            self.mode = SpongeMode.Absorbing;
+            self.offset = 0;
+        } else if (self.offset == RATE) {
+            self.state = Poseidon.permutation(self.state);
+            self.offset = 0;
+        }
+
+        self.state[self.offset] = self.state[self.offset].add(elem);
+        self.offset += 1;
+    }
+
+    function squeeze(BaseSponge memory self) public pure returns (Base.FE digest){
+        if (self.mode == SpongeMode.Absorbing || self.offset == RATE) {
+            self.mode = SpongeMode.Squeezing;
+            self.state = Poseidon.permutation(self.state);
+            self.offset = 0;
+        }
+
+        digest = self.state[self.offset];
+        self.offset += 1;
     }
 }
