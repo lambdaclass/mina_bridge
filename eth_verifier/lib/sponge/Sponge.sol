8a5c688b7b1b86c0a5d5ce11774d8cf446305dfb
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2023-11-16 13:52:14 -0300

    Replace poseidon with keccak sponge

---
+++ eth_verifier/lib/sponge/Sponge.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+import "../bn254/Fields.sol";
+import "../bn254/BN254.sol";
+
+struct Sponge {
+    bytes state;
+}
+
+library KeccakSponge {
+    // Basic methods
+
+    function init() external pure returns (Sponge memory) {
+        return Sponge(new bytes(0));
+    }
+
+    function absorb(Sponge storage self, bytes memory b) external {
+        for (uint i = 0; i < b.length; i++) {
+            self.state.push(b[i]);
+        }
+    }
+
+    function squeeze(
+        Sponge memory self,
+        uint byte_count
+    ) public pure returns (bytes memory digest) {
+        digest = new bytes(byte_count);
+        bytes32 output;
+
+        for (uint i = 0; i < byte_count; i++) {
+            if (i % 32 == 0) {
+                output = keccak256(self.state);
+                self.state = abi.encode(output);
+            }
+
+            digest[i] = output[i % 32];
+        }
+    }
+
+    // KZG methods
+
+    function absorb_base(Sponge storage self, Base.FE elem) external {
+        bytes memory b = abi.encode(elem);
+        for (uint i = 0; i < b.length; i++) {
+            self.state.push(b[i]);
+        }
+    }
+
+    function absorb_scalar(Sponge storage self, Scalar.FE elem) external {
+        bytes memory b = abi.encode(elem);
+        for (uint i = 0; i < b.length; i++) {
+            self.state.push(b[i]);
+        }
+    }
+
+    function absorb_g(
+        Sponge storage self,
+        BN254.G1Point memory point
+    ) external {
+        bytes memory b = abi.encode(point);
+        for (uint i = 0; i < b.length; i++) {
+            self.state.push(b[i]);
+        }
+    }
+
+    function challenge_base(
+        Sponge storage self
+    ) external pure returns (Base.FE chal) {
+        uint256 integer = abi.decode(squeeze(self, 16), (uint256));
+        chal = Base.from(integer);
+    }
+
+    function challenge_scalar(
+        Sponge storage self
+    ) external pure returns (Scalar.FE chal) {
+        uint256 integer = abi.decode(squeeze(self, 16), (uint256));
+        chal = Scalar.from(integer);
+    }
+
+    function digest_base(
+        Sponge storage self
+    ) external pure returns (Base.FE digest) {
+        uint256 integer = abi.decode(squeeze(self, 32), (uint256));
+        digest = Base.from(integer);
+    }
+
+    function digest_scalar(
+        Sponge storage self
+    ) external pure returns (Scalar.FE digest) {
+        uint256 integer = abi.decode(squeeze(self, 32), (uint256));
+        digest = Scalar.from(integer);
+    }
+}
