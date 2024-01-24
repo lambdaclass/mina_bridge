d0fb89f9efc900d9e9d5cf0e703a68f6e23658b6
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-01-24 15:59:53 -0300

    Update eth_verifier/script/Verify.s.sol
    Co-authored-by: Pablo Deymonnaz <pdeymon@fi.uba.ar>

--- eth_verifier/script/Verify.s.sol
+++ eth_verifier/script/Verify.s.sol
@@ -26,7 +26,6 @@ contract Verify is Script {
             numerator_binary
         );
 
-        //console.log("Verification ", success ? "succeeded" : "failed");
         require(success, "Verification failed.");
 
         vm.stopBroadcast();
