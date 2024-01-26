f03c233f1905c89023c5b50ed81b4acdadcaaa67
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-01-26 17:33:03 -0300

    Added absorb_x_base tests

--- eth_verifier/test/Sponge.t.sol
+++ eth_verifier/test/Sponge.t.sol
@@ -9,7 +9,10 @@ using {
     KeccakSponge.reinit,
     KeccakSponge.absorb_scalar,
     KeccakSponge.digest_scalar,
-    KeccakSponge.challenge_scalar
+    KeccakSponge.challenge_scalar,
+    KeccakSponge.absorb_base,
+    KeccakSponge.digest_base,
+    KeccakSponge.challenge_base
 } for Sponge;
 
 contract KeccakSpongeTest is Test {
@@ -36,6 +39,34 @@ contract KeccakSpongeTest is Test {
         Scalar.FE digest = sponge.challenge_scalar();
         assertEq(
             Scalar.FE.unwrap(digest),
+            0x0000000000000000000000000000000000BECED09521047D05B8960B7E7BCC1D
+        );
+        // INFO: reference value taken from analogous test in kzg_prover/sponge_tests.rs
+    }
+
+    // INFO: absorb_x_base tests are the same as absorb_x_scalar ones.
+    function test_absorb_digest_base() public {
+        sponge.reinit();
+        Base.FE input = Base.from(42);
+        sponge.absorb_base(input);
+
+        Base.FE digest = sponge.digest_base();
+        assertEq(
+            Base.FE.unwrap(digest),
+            0x00BECED09521047D05B8960B7E7BCC1D1292CF3E4B2A6B63F48335CBDE5F7545
+        );
+        // INFO: reference value taken from analogous test in kzg_prover/sponge_tests.rs
+    }
+
+    // INFO: absorb_x_base tests are the same as absorb_x_scalar ones.
+    function test_absorb_challenge_base() public {
+        sponge.reinit();
+        Base.FE input = Base.from(42);
+        sponge.absorb_base(input);
+
+        Base.FE digest = sponge.challenge_base();
+        assertEq(
+            Base.FE.unwrap(digest),
             0x0000000000000000000000000000000000BECED09521047D05B8960B7E7BCC1D
         );
         // INFO: reference value taken from analogous test in kzg_prover/sponge_tests.rs
