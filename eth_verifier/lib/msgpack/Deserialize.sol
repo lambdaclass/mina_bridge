513ef430a23c1a1f538ed6a62ab2f923e885db74
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-02-02 18:00:36 -0300

    Remove unused Kimchi library

--- eth_verifier/lib/msgpack/Deserialize.sol
+++ eth_verifier/lib/msgpack/Deserialize.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.16 <0.9.0;
 
-import {Kimchi} from "../../src/Verifier.sol";
 import "../Commitment.sol";
 import "../bn254/BN254.sol";
 import "../Evaluations.sol";
--- eth_verifier/src/Verifier.sol
+++ eth_verifier/src/Verifier.sol
@@ -22,35 +22,6 @@ using {Scalar.neg, Scalar.mul, Scalar.add, Scalar.inv, Scalar.sub, Scalar.pow} f
 using {AlphasLib.get_alphas} for Alphas;
 using {Polynomial.evaluate} for Polynomial.Dense;
 
-library Kimchi {
-    struct Proof {
-        uint256 data;
-    }
-
-    struct ProofInput {
-        uint256[] serializedProof;
-    }
-
-    struct ProverProof {
-        // evals
-
-        // opening proof
-        BN254.G1Point opening_proof_quotient;
-        uint256 opening_proof_blinding;
-    }
-
-    struct Evals {
-        Base.FE zeta;
-        Base.FE zeta_omega;
-    }
-
-    /*
-    function deserializeEvals(
-        uint8[71] calldata serialized_evals
-    ) public view returns (Evals memory evals) {}
-    */
-}
-
 contract KimchiVerifier {
     using {AlphasLib.register} for Alphas;
     using {combine_evals} for ProofEvaluationsArray;
