7edbb5cdd602ef044c8e711ec17977e1b72b7e49
gabrielbosio <gabrielbosio95@gmail.com>
2024-03-15 14:32:33 -0300

    Add updated verifier script

---
+++ eth_verifier/run.sh
@@ -0,0 +1,9 @@
+(anvil --gas-limit 18446744073709551615 --code-size-limit 32768 &)
+
+echo "Waiting for local blockchain..."
+sleep 3
+
+make verify
+
+echo "After interacting with your contract, don't forget to kill the Anvil process:"
+echo "    pkill anvil"
