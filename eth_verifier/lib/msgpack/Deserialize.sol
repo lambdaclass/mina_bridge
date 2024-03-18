8bba44ecae6f2b351069cf6b2f78385e7769323a
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-03-18 17:48:24 -0300

    Fixed uint deser

--- eth_verifier/lib/msgpack/Deserialize.sol
+++ eth_verifier/lib/msgpack/Deserialize.sol
@@ -946,12 +946,12 @@ library MsgPk {
         }
         (bytes memory coefficient_value, bool is_coefficient) = find_value_or_fail(col_map, abi.encode("Coefficient"));
         if (is_coefficient) {
-            uint256 i = deser_uint(new_stream(coefficient_value));
+            uint256 i = abi.decode(coefficient_value, (uint256));
             return Column(ColumnVariant.Coefficient, abi.encode(i));
         }
         (bytes memory permutation_value, bool is_permutation) = find_value_or_fail(col_map, abi.encode("Permutation"));
         if (is_permutation) {
-            uint256 i = deser_uint(new_stream(permutation_value));
+            uint256 i = abi.decode(permutation_value, (uint256));
             return Column(ColumnVariant.Permutation, abi.encode(i));
         }
         revert("Couldn't match any Column variant while deserializing a column.");
