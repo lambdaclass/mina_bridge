e8ca7d8315d6792ee4d55a57fae7629bc5eafc6f
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-03-20 17:21:58 -0300

    Rethinked serializer

--- kzg_prover/src/serialize.rs
+++ kzg_prover/src/serialize.rs
@@ -1,25 +1,32 @@
 use ark_ec::short_weierstrass_jacobian::GroupAffine;
+use o1_utils::FieldHelpers;
 use poly_commitment::pairing_proof::PairingProof;
 
-type BaseField = ark_bn254::Fq;
-type KZGProof = PairingProof<ark_ec::bn::Bn<ark_bn254::Parameters>>;
-type G1Point = GroupAffine<ark_bn254::g1::Parameters>;
-
-struct G1PointSchema {
-    x: BaseField,
-    y: BaseField,
+pub trait Serializable {
+    fn to_bytes(self) -> Vec<u8>;
 }
 
-impl From<G1Point> for G1PointSchema {
-    fn from(value: G1Point) -> Self {
-        if value.infinity {
-            G1PointSchema {
-                x: BaseField::from(0),
-                y: BaseField::from(0),
-            }
+type BN254PairingProof = PairingProof<ark_ec::bn::Bn<ark_bn254::Parameters>>;
+type G1Point = GroupAffine<ark_bn254::g1::Parameters>;
+
+struct G1PointSerializable(G1Point);
+struct PairingProofSerializable(BN254PairingProof);
+
+impl Serializable for G1PointSerializable {
+    fn to_bytes(self) -> Vec<u8> {
+        let GroupAffine { x, y, infinity, .. } = self.0;
+        if infinity {
+            vec![0; 64]
         } else {
-            let G1Point { x, y, .. } = value;
-            G1PointSchema { x, y }
+            [x.to_bytes(), y.to_bytes()].concat()
         }
+    }
+}
+
+impl Serializable for PairingProofSerializable {
+    fn to_bytes(self) -> Vec<u8> {
+        let BN254PairingProof { quotient, blinding } = self.0;
+        let quotient = G1PointSerializable(quotient);
+        [quotient.to_bytes(), blinding.to_bytes()].concat()
     }
 }
