91817eb3f89186d41b1918ba5bfed7f16ab1d03a
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-03-25 14:24:28 -0300

    bug fixed in KimchiVerifierDemo

--- eth_verifier/script/Deploy.s.sol
+++ eth_verifier/script/Deploy.s.sol
@@ -22,7 +22,7 @@ contract DeployAndVerify is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        KimchiVerifierDemo verifier = new KimchiVerifier();
+        KimchiVerifierDemo verifier = new KimchiVerifierDemo();
 
         bool success = verifier.verify_state(vm.readFileBinary("state.mpk"), vm.readFileBinary("proof.mpk"));
         console.log(success);
