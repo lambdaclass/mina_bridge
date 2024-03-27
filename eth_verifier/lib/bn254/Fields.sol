e9c6cfcb03421e8cec654ca3fdb94f28f954fc7b
gabrielbosio <gabrielbosio95@gmail.com>
2024-03-27 11:38:44 -0300

    Apply precompile usage for both scalar and base

--- eth_verifier/lib/bn254/Fields.sol
+++ eth_verifier/lib/bn254/Fields.sol
@@ -68,15 +68,8 @@ library Base {
         }
     }
 
-    function pow(FE self, uint256 exponent) public pure returns (FE result) {
-        result = FE.wrap(1);
-        while (exponent != 0) {
-            if (exponent & 1 == 1) {
-                result = mul(result, self);
-            }
-            self = mul(self, self);
-            exponent = exponent >> 1;
-        }
+    function pow(FE self, uint256 exponent) public view returns (FE result) {
+        result = FE.wrap(Aux.expmod(FE.unwrap(self), exponent, MODULUS));
     }
 }
 
@@ -152,24 +145,7 @@ library Scalar {
     }
 
     function pow(FE self, uint256 exponent) public view returns (FE result) {
-        result = FE.wrap(expmod(FE.unwrap(self), exponent, MODULUS));
-    }
-
-    function expmod(uint256 base, uint256 e, uint256 m) internal view returns (uint256 o) {
-        assembly {
-            // define pointer
-            let p := mload(0x40)
-            // store data assembly-favouring ways
-            mstore(p, 0x20) // Length of Base
-            mstore(add(p, 0x20), 0x20) // Length of Exponent
-            mstore(add(p, 0x40), 0x20) // Length of Modulus
-            mstore(add(p, 0x60), base) // Base
-            mstore(add(p, 0x80), e) // Exponent
-            mstore(add(p, 0xa0), m) // Modulus
-            if iszero(staticcall(sub(gas(), 2000), 0x05, p, 0xc0, p, 0x20)) { revert(0, 0) }
-            // data
-            o := mload(p)
-        }
+        result = FE.wrap(Aux.expmod(FE.unwrap(self), exponent, MODULUS));
     }
 
     error RootOfUnityError();
@@ -238,6 +214,23 @@ library Aux {
             s0 = b - s0;
         } else {
             t0 = a - t0;
+        }
+    }
+
+    function expmod(uint256 base, uint256 e, uint256 m) internal view returns (uint256 o) {
+        assembly {
+            // define pointer
+            let p := mload(0x40)
+            // store data assembly-favouring ways
+            mstore(p, 0x20) // Length of Base
+            mstore(add(p, 0x20), 0x20) // Length of Exponent
+            mstore(add(p, 0x40), 0x20) // Length of Modulus
+            mstore(add(p, 0x60), base) // Base
+            mstore(add(p, 0x80), e) // Exponent
+            mstore(add(p, 0xa0), m) // Modulus
+            if iszero(staticcall(sub(gas(), 2000), 0x05, p, 0xc0, p, 0x20)) { revert(0, 0) }
+            // data
+            o := mload(p)
         }
     }
 }
