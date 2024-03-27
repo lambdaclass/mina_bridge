ffe45e5251fe89f7de6b2b7722b33294e9c4cfe2
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-03-27 13:03:54 -0300

    Refactored tests, added comm test

--- eth_verifier/test/Deserialize.t.sol
+++ eth_verifier/test/Deserialize.t.sol
@@ -5,13 +5,16 @@ import {Test, console2} from "forge-std/Test.sol";
 import "../lib/deserialize/PairingProof.sol";
 import "../lib/deserialize/ProverProof.sol";
 import "../lib/Proof.sol";
+import "../lib/Constants.sol";
 
 contract DeserializeTest is Test {
     bytes pairing_proof_bytes;
     bytes proof_evals_bytes;
+    bytes proof_comms_bytes;
 
     NewPairingProof pairing_proof;
     NewProofEvaluations proof_evals;
+    NewProverCommitments proof_comms;
 
     function setUp() public {
         pairing_proof_bytes = vm.readFileBinary(
@@ -20,13 +23,30 @@ contract DeserializeTest is Test {
         proof_evals_bytes = vm.readFileBinary(
             "./unit_test_data/proof_evals.bin"
         );
+        proof_comms_bytes = vm.readFileBinary(
+            "./unit_test_data/proof_comms.bin"
+        );
+    }
+
+    function assertTestEval(PointEvaluations memory eval) internal {
+        assertEq(Scalar.FE.unwrap(eval.zeta), 1);
+        assertEq(Scalar.FE.unwrap(eval.zeta_omega), 42);
+    }
+
+    function assertEmptyEval(PointEvaluations memory eval) internal {
+        assertEq(Scalar.FE.unwrap(eval.zeta), 0);
+        assertEq(Scalar.FE.unwrap(eval.zeta_omega), 0);
+    }
+
+    function assertTestG1Point(BN254.G1Point memory p) internal {
+        assertEq(p.x, 1);
+        assertEq(p.y, 2);
     }
 
     function test_deser_new_pairing_proof() public {
         deser_pairing_proof(pairing_proof_bytes, pairing_proof);
 
-        assertEq(pairing_proof.quotient.x, 1);
-        assertEq(pairing_proof.quotient.y, 2);
+        assertTestG1Point(pairing_proof.quotient);
         assertEq(Scalar.FE.unwrap(pairing_proof.blinding), 1);
     }
 
@@ -40,37 +60,24 @@ contract DeserializeTest is Test {
         );
 
         // will only test some fields:
+        assertTestEval(proof_evals.z);
+        assertTestEval(proof_evals.public_evals);
+        assertTestEval(proof_evals.range_check0_selector);
+        assertEmptyEval(proof_evals.range_check1_selector);
+        assertTestEval(proof_evals.foreign_field_add_selector);
+    }
 
-        // z
-        assertEq(Scalar.FE.unwrap(proof_evals.z.zeta), 1);
-        assertEq(Scalar.FE.unwrap(proof_evals.z.zeta_omega), 42);
+    function test_deser_new_proof_comms() public {
+        deser_proof_comms(proof_comms_bytes, proof_comms);
 
-        // public_evals
-        assertEq(Scalar.FE.unwrap(proof_evals.public_evals.zeta), 1);
-        assertEq(Scalar.FE.unwrap(proof_evals.public_evals.zeta_omega), 42);
-
-        // range_check0_selector
-        assertEq(Scalar.FE.unwrap(proof_evals.range_check0_selector.zeta), 1);
+        // optional field flags
         assertEq(
-            Scalar.FE.unwrap(proof_evals.range_check0_selector.zeta_omega),
-            42
+            bitmap.unwrap(proof_comms.optional_field_flags),
+            3 // 0b11
         );
 
-        // range_check1_selector
-        assertEq(Scalar.FE.unwrap(proof_evals.range_check1_selector.zeta), 0);
-        assertEq(
-            Scalar.FE.unwrap(proof_evals.range_check1_selector.zeta_omega),
-            0
-        );
-
-        // foreign_field_add_selector
-        assertEq(
-            Scalar.FE.unwrap(proof_evals.foreign_field_add_selector.zeta),
-            1
-        );
-        assertEq(
-            Scalar.FE.unwrap(proof_evals.foreign_field_add_selector.zeta_omega),
-            42
-        );
+        for (uint256 i = 0; i < COLUMNS; i++) {
+            assertTestG1Point(proof_comms.w_comm[i]);
+        }
     }
 }
