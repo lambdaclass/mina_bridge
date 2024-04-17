a410ad65a5a31fc40c02b41321acfa6446cbc750
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-04-17 15:56:10 -0300

    Fix gas profiling tests (#207)
    * Fix gas profiling tests
    
    * Change contract name
    
    * Fix profiling pub input test

--- eth_verifier/test/MessagePack.t.sol
+++ eth_verifier/test/MessagePack.t.sol
@@ -102,25 +102,4 @@ contract MessagePackTest is Test {
         assertEq(point.y0, y0, "y0 is not correct");
         assertEq(point.y1, y1, "y1 is not correct");
     }
-
-    // INFO: this doesn't assert anything, it only executes this deserialization
-    // for gas profiling.
-    function test_deserialize_linearization_profiling_only() public {
-        bytes memory linearization_serialized = vm.readFileBinary("linearization.mpk");
-        MsgPk.deser_linearization(MsgPk.new_stream(linearization_serialized), index);
-    }
-
-    // INFO: this doesn't assert anything, it only executes this deserialization
-    // for gas profiling.
-    function test_decode_linearization_profiling_only() public {
-        bytes memory linearization_rlp = vm.readFileBinary("linearization.rlp");
-        abi.decode(linearization_rlp, (Linearization));
-    }
-
-    // INFO: this doesn't assert anything, it only executes this deserialization
-    // for gas profiling.
-    function test_deserialize_verifier_index_profiling_only() public {
-        bytes memory verifier_index_serialized = vm.readFileBinary("verifier_index.mpk");
-        deser_verifier_index(verifier_index_serialized, index);
-    }
 }
