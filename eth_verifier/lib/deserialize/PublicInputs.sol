727fb02425467cfae9c005b15cccb385f2b5e035
gabrielbosio <gabrielbosio95@gmail.com>
2024-04-19 18:48:49 -0300

    Simplify deserialization

--- eth_verifier/lib/deserialize/PublicInputs.sol
+++ eth_verifier/lib/deserialize/PublicInputs.sol
@@ -6,13 +6,11 @@ import "../bn254/Fields.sol";
 import "forge-std/console.sol";
 
 function deser_public_input(bytes memory data) pure returns (Scalar.FE public_input) {
-    uint256 inner;
     assembly ("memory-safe") {
         // first 32 bytes is the length of the bytes array, we'll skip them.
         let addr := add(data, 0x20)
 
         // Store public input
-        inner := mload(addr)
+        public_input := mload(addr)
     }
-    public_input = Scalar.FE.wrap(inner);
 }
