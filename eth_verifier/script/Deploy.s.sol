8bafb12f9d998cdf44fb1a054347e39448c79236
Gabriel Bosio <38794644+gabrielbosio@users.noreply.github.com>
2024-05-02 17:38:07 -0300

    API of verification over testnet  (#227)
    * Add function to get verification result
    
    * Add everything necessary for deployment into sepolia and contract verification
    
    * Improve final verify name fn
    
    * Add verification make cmd
    
    * Separate proof upload and verifier deployment
    
    * Document verification on testnet
    
    * Rename sepolia commands
    
    * Move linearization and public input to proof upload
    
    * Documents what it's uploaded on UploadProof script
    
    ---------
    
    Co-authored-by: Estéfano Bargas <estefano.bargas@fing.edu.uy>
    Co-authored-by: gabrielbosio <gabriel.bosio@lambdaclass.com>

--- eth_verifier/script/Deploy.s.sol
+++ eth_verifier/script/Deploy.s.sol
@@ -5,7 +5,6 @@ import {Script, console2} from "forge-std/Script.sol";
 import {KimchiVerifier} from "../src/Verifier.sol";
 
 contract Deploy is Script {
-
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
@@ -18,17 +17,12 @@ contract Deploy is Script {
         vm.stopBroadcast();
     }
 }
+
 contract DeployAndDeser is Script {
     bytes verifier_index_serialized;
-    bytes prover_proof_serialized;
-    bytes linearization_serialized;
-    bytes public_input_serialized;
 
     function run() public {
         verifier_index_serialized = vm.readFileBinary("verifier_index.bin");
-        prover_proof_serialized = vm.readFileBinary("prover_proof.bin");
-        linearization_serialized = vm.readFileBinary("linearization.bin");
-        public_input_serialized = vm.readFileBinary("public_input.bin");
 
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
@@ -39,9 +33,6 @@ contract DeployAndDeser is Script {
         verifier.setup();
 
         verifier.store_verifier_index(verifier_index_serialized);
-        verifier.store_linearization(linearization_serialized);
-        verifier.store_prover_proof(prover_proof_serialized);
-        verifier.store_public_input(public_input_serialized);
 
         vm.stopBroadcast();
     }
