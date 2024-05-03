611bfc8f3b4e14d67d21cfeca8b31b0b50ddd2b9
gabrielbosio <gabriel.bosio@lambdaclass.com>
2024-05-03 18:24:34 -0300

    Check that account belongs to Mina state

--- state_utility/run.sh
+++ state_utility/run.sh
@@ -1,9 +1,30 @@
-# This script receives an argument with the public key of the user account and queries the last 
-# balance associated
-PGPASSWORD=postgres psql -qtA -h localhost -p 5432 -U postgres -d minanode -c "
-select balance
-from balances as b
-inner join public_keys as pk on b.public_key_id = pk.id
-where pk.value = '$1'
-order by block_height desc limit 1;
-"
+#!/bin/bash
+# Receives a Mina public key and a Mina state hash.
+# Checks that the Mina public key belongs to the Mina state related to the passed hash. 
+
+IFS='
+'
+export PGPASSWORD=postgres
+FOUND=0
+
+RET_SQL=`psql -qtA -h localhost -p 5432 -U postgres -d minanode -c "
+select state_hash 
+from blocks
+inner join balances on blocks.id = balances.block_id
+inner join public_keys on balances.public_key_id = public_keys.id
+where public_keys.value = '$1';
+"`
+for state_hash in $RET_SQL
+do
+    if [ "$state_hash" = "$2" ]
+    then
+        echo "Mina public key belongs to the Mina state!"
+        FOUND=1
+        break
+    fi
+done
+
+if [ $FOUND -eq 0 ]; then
+    echo "Error: Mina public key does not belong to the Mina state"
+    exit 1
+fi
