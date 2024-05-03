1969ef548044e117bfd1c9939f51a4720769bd9c
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-05-03 15:46:39 -0300

    Make Utils functions internal

--- eth_verifier/lib/Utils.sol
+++ eth_verifier/lib/Utils.sol
@@ -10,19 +10,19 @@ using {Scalar.pow, Scalar.inv, Scalar.add, Scalar.mul, Scalar.neg} for Scalar.FE
 
 library Utils {
     /// @notice returns minimum between a and b.
-    function min(uint256 a, uint256 b) public pure returns (uint256) {
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
         return a < b ? a : b;
     }
 
     /// @notice returns maximum between a and b.
-    function max(uint256 a, uint256 b) public pure returns (uint256) {
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
         return a < b ? b : a;
     }
 
     /// @notice converts an ASCII string into a uint.
     error InvalidStringToUint();
 
-    function str_to_uint(string memory s) public pure returns (uint256 res) {
+    function str_to_uint(string memory s) internal pure returns (uint256 res) {
         bytes memory b = bytes(s);
         res = 0;
         for (uint256 i = 0; i < b.length; i++) {
@@ -39,7 +39,7 @@ library Utils {
     /// @notice assumed to be padded so every element is 32 bytes long.
     //
     // @notice this function will both flat and remove the padding.
-    function flatten_padded_be_bytes_array(bytes[] memory b) public pure returns (bytes memory) {
+    function flatten_padded_be_bytes_array(bytes[] memory b) internal pure returns (bytes memory) {
         uint256 byte_count = b.length;
         bytes memory flat_b = new bytes(byte_count);
         for (uint256 i = 0; i < byte_count; i++) {
@@ -52,7 +52,7 @@ library Utils {
     /// @notice assumed to be padded so every element is 32 bytes long.
     //
     // @notice this function will both flat and remove the padding.
-    function flatten_padded_le_bytes_array(bytes[] memory b) public pure returns (bytes memory) {
+    function flatten_padded_le_bytes_array(bytes[] memory b) internal pure returns (bytes memory) {
         uint256 byte_count = b.length;
         bytes memory flat_b = new bytes(byte_count);
         for (uint256 i = 0; i < byte_count; i++) {
@@ -63,7 +63,7 @@ library Utils {
 
     /// @notice uses `flatten_padded_bytes_array()` to flat and remove the padding
     /// @notice of a `bytes[]` and reinterprets the result as a big-endian uint256.
-    function padded_be_bytes_array_to_uint256(bytes[] memory b) public pure returns (uint256 integer) {
+    function padded_be_bytes_array_to_uint256(bytes[] memory b) internal pure returns (uint256 integer) {
         bytes memory data_b = flatten_padded_be_bytes_array(b);
         require(data_b.length == 32, "not enough bytes in array");
         return uint256(bytes32(data_b));
@@ -71,14 +71,14 @@ library Utils {
 
     /// @notice uses `flatten_padded_bytes_array()` to flat and remove the padding
     /// @notice of a `bytes[]` and reinterprets the result as a little-endian uint256.
-    function padded_le_bytes_array_to_uint256(bytes[] memory b) public pure returns (uint256 integer) {
+    function padded_le_bytes_array_to_uint256(bytes[] memory b) internal pure returns (uint256 integer) {
         bytes memory data_b = flatten_padded_le_bytes_array(b);
         require(data_b.length == 32, "not enough bytes in array");
         return uint256(bytes32(data_b));
     }
 
     /// @notice checks if two strings are equal
-    function str_cmp(string memory self, string memory other) public pure returns (bool) {
+    function str_cmp(string memory self, string memory other) internal pure returns (bool) {
         return keccak256(bytes(self)) == keccak256(bytes(other));
     }
 }
