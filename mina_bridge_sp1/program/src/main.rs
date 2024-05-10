c2c04f5923ba3996a117d8509222b7a27537ff17
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-05-10 17:22:34 -0300

    Finished verification circuit

--- mina_bridge_sp1/program/src/main.rs
+++ mina_bridge_sp1/program/src/main.rs
@@ -1,22 +1,28 @@
 //! A simple program to be proven inside the zkVM.
 
 #![no_main]
+
+use kimchi::{
+    groupmap::GroupMap,
+    mina_curves::pasta::Vesta,
+    poly_commitment::{commitment::CommitmentCurve, evaluation_proof::OpeningProof},
+    proof::ProverProof,
+    verifier_index::VerifierIndex,
+};
+use kimchi_verifier_ffi::kimchi_verify;
+
 sp1_zkvm::entrypoint!(main);
 
-pub fn main() {
-    // NOTE: values of n larger than 186 will overflow the u128 type,
-    // resulting in output that doesn't match fibonacci sequence.
-    // However, the resulting proof will still be valid!
-    let n = sp1_zkvm::io::read::<u32>();
-    let mut a: u128 = 0;
-    let mut b: u128 = 1;
-    let mut sum: u128;
-    for _ in 1..n {
-        sum = a + b;
-        a = b;
-        b = sum;
-    }
+type Curve = Vesta;
+type KimchiOpeningProof = OpeningProof<Curve>;
+type KimchiProof = ProverProof<Curve, KimchiOpeningProof>;
+type KimchiVerifierIndex = VerifierIndex<Curve, KimchiOpeningProof>;
 
-    sp1_zkvm::io::commit(&a);
-    sp1_zkvm::io::commit(&b);
+pub fn main() {
+    let proof = sp1_zkvm::io::read::<KimchiProof>();
+    let verifier_index = sp1_zkvm::io::read::<KimchiVerifierIndex>();
+    let group_map = <Curve as CommitmentCurve>::Map::setup();
+
+    let result = kimchi_verify(proof, verifier_index, group_map);
+    sp1_zkvm::io::commit(&result);
 }
