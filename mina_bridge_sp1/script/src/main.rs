ce6928d1ee5e56b7c786c350f55f01aa275fef0d
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-05-13 10:59:34 -0300

    Use prove_compressed

--- mina_bridge_sp1/script/src/main.rs
+++ mina_bridge_sp1/script/src/main.rs
@@ -6,6 +6,9 @@ use kimchi_verifier_ffi::generate_test_proof;
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
 fn main() {
+    // Setup the logger.
+    sp1_sdk::utils::setup_logger();
+
     // Generate proof.
     let mut stdin = SP1Stdin::new();
     let (proof, index, srs) = generate_test_proof();
@@ -14,21 +17,19 @@ fn main() {
     stdin.write(&srs);
     let client = ProverClient::new();
     let (pk, vk) = client.setup(ELF);
-    let mut proof = client.prove(&pk, stdin).expect("proving failed");
+    let mut proof = client.prove_compressed(&pk, stdin).expect("proving failed");
 
     // Read output.
     let result = proof.public_values.read::<bool>();
     println!("Verification result: {}", result);
 
-    /*
-    // Verify proof.
-    client.verify(&proof, &vk).expect("verification failed");
-    */
-
     // Save proof.
     proof
         .save("proof-with-io.json")
         .expect("saving proof failed");
+
+    // Verify proof.
+    client.verify_compressed(&proof, &vk).expect("verification failed");
 
     println!("successfully generated and verified proof for the program!")
 }
