3cf53ba89715bcb3733345fe06ac9278ac6e592d
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-05-24 18:11:23 -0300

    pairing function embedded

--- eth_verifier/lib/bn254/BN254.sol
+++ eth_verifier/lib/bn254/BN254.sol
@@ -22,7 +22,6 @@ error LengthDoesNotMatch(); // Bn254: MSM length does not match!
 error PowPrecompileFailed(); // Bn254: pow precompile failed!
 error InvalidG1Point(); // Bn254: invalid G1 point!
 error InvalidScalarField(); // Bn254: invalid scalar field!
-error PairingCheckFailed(); // Bn254: pairing check failed!
 
 /// @notice Barreto-Naehrig curve over a 254 bit prime field
 library BN254 {
@@ -49,11 +48,6 @@ library BN254 {
     // BN254 is defined over a 254-bit prime order p, embedding degree k = 12.
     uint256 internal constant P_MOD = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
     uint256 internal constant R_MOD = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
-
-    uint256 internal constant G2_X0 = 0x198e9393920d483a7260bfb731fb5d25f1aa493335a9e71297e485b7aef312c2;
-    uint256 internal constant G2_X1 = 0x1800deef121f1e76426a00665e5c4479674322d4f75edadd46debd5cd992f6ed;
-    uint256 internal constant G2_Y0 = 0x090689d0585ff075ec9e99ad690c3395bc4b313370b38ef355acdadcd122975b;
-    uint256 internal constant G2_Y1 = 0x12c85ea5db8c6deb4aab71808dcb408fe3d1e7690c43d37b4ce6cc0166fa7daa;
 
     using {add, scalarMul} for G1Point;
 
@@ -263,38 +257,6 @@ library BN254 {
         if (!isValid) {
             revert InvalidScalarField();
         }
-    }
-
-    /// @dev Evaluate the following pairing product:
-    /// @dev e(a1, a2).e(-b1, b2) == 1
-    /// @dev caller needs to ensure that a1, a2, b1 and b2 are within proper group
-    /// @notice credit: Aztec, Spilsbury Holdings Ltd
-    function pairingProd2(G1Point memory a1, G1Point memory b1, G2Point memory b2) internal view returns (bool) {
-        uint256 out;
-        bool success;
-        assembly ("memory-safe") {
-            let mPtr := mload(0x40)
-            mstore(mPtr, mload(a1))
-            mstore(add(mPtr, 0x20), mload(add(a1, 0x20)))
-
-            mstore(add(mPtr, 0x40), G2_X0)
-            mstore(add(mPtr, 0x60), G2_X1)
-            mstore(add(mPtr, 0x80), G2_Y0)
-            mstore(add(mPtr, 0xa0), G2_Y1)
-
-            mstore(add(mPtr, 0xc0), mload(b1))
-            mstore(add(mPtr, 0xe0), mload(add(b1, 0x20)))
-            mstore(add(mPtr, 0x100), mload(b2))
-            mstore(add(mPtr, 0x120), mload(add(b2, 0x20)))
-            mstore(add(mPtr, 0x140), mload(add(b2, 0x40)))
-            mstore(add(mPtr, 0x160), mload(add(b2, 0x60)))
-            success := staticcall(gas(), 8, mPtr, 0x180, 0x00, 0x20)
-            out := mload(0x00)
-        }
-        if (!success) {
-            revert PairingCheckFailed();
-        }
-        return (out != 0);
     }
 
     function fromLeBytesModOrder(bytes memory leBytes) internal pure returns (uint256 ret) {
