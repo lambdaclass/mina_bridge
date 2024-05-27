6798f3d7c1cb70a7d2929c2dc6250e57312e662b
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-05-27 16:28:20 -0300

    Prove fibonacci

--- mina_bridge_sp1/prove/src/main.rs
+++ mina_bridge_sp1/prove/src/main.rs
@@ -1,10 +1,11 @@
+
 //! A simple script to generate and verify the proof of a given program.
 
 use std::fs::File;
 
 use kimchi_verifier_ffi::generate_test_proof;
 use sp1_sdk::{
-    artifacts::get_groth16_artifacts_dir, ProverClient, SP1CompressedProof,
+    artifacts::try_install_groth16_artifacts, ProverClient, SP1CompressedProof,
     SP1ProofWithPublicValues, SP1Stdin,
 };
 
@@ -16,52 +17,43 @@ fn main() {
 
     // Generate test Kimchi proof.
     let mut stdin = SP1Stdin::new();
-    let (proof, index, srs) = generate_test_proof();
 
     // Write test Kimchi proof to stdin
-    stdin.write(&proof);
-    stdin.write(&index);
-    stdin.write(&srs);
-    stdin.write(&srs.lagrange_bases);
+    stdin.write(&10000);
 
-    // Setup SP1 client
+    // Setup SP1 client and prover
     let client = ProverClient::new();
     let (pk, vk) = client.setup(ELF);
+    let prover = client.prover.sp1_prover();
 
     // Prove program execution
     let prover = client.prover.sp1_prover();
-    let mut proof = prover.prove_core(&pk, &stdin);
+    let mut proof = prover.prove_core(&pk, &stdin).unwrap();
 
-    // Read output.
-    let result = proof.public_values.read::<bool>();
-    println!("Verification result: {}", result);
+    /* Temporarily disable for faster iteration
+        let proof_with_public = SP1ProofWithPublicValues {
+            proof: proof.proof.0.clone(),
+            stdin: proof.stdin.clone(),
+            public_values: proof.public_values.clone(),
+        };
 
-    // Save proof with metadata
-    proof
-        .save("proof_with_metadata.bin")
-        .expect("saving proof with metadata failed");
+        println!("Verifying shard proof...");
+        // Verify proof.
+        client
+            .verify(&proof_with_public, &vk)
+            .expect("proof with public verification failed");
+        println!("Proof was verified!");
+    */
 
-    // Save proof with public values (this is the result of a normal JSON proof)
-    // without compression or wrapping.
-    let proof_with_public = SP1ProofWithPublicValues {
-        proof: proof.proof.0.clone(),
-        stdin: proof.stdin.clone(),
-        public_values: proof.public_values.clone(),
-    };
-    proof_with_public
-        .save("proof_with_public.json")
-        .expect("saving proof with public failed");
-
-    // Verify proof.
-    client
-        .verify(&proof_with_public, &vk)
-        .expect("proof with public verification failed");
-
+    println!("Compressing proof");
     // Compress proof
     let deferred_proofs = stdin.proofs.iter().map(|p| p.0.clone()).collect();
     let public_values = proof.public_values.clone();
-    let reduce_proof = prover.compress(&pk.vk, proof, deferred_proofs);
+    let reduce_proof = prover
+        .compress(&pk.vk, proof, deferred_proofs)
+        .expect("compression failed");
 
+    println!("Saving compressed proof");
     // Save reduce proof
     bincode::serialize_into(
         File::create("reduce_proof.bin").expect("failed to open file"),
@@ -79,13 +71,18 @@ fn main() {
     .save("reduce_compressed_proof.json")
     .expect("saving reduce compressed proof failed");
 
+    println!("Shrink proof");
     // Compress and wrap proof over SNARK-friendly bn254
-    let compress_proof = prover.shrink(reduce_proof);
-    let outer_proof = prover.wrap_bn254(compress_proof);
+    let compress_proof = prover.shrink(reduce_proof).expect("shrink failed");
+    println!("Wrap proof in BN254");
+    let outer_proof = prover
+        .wrap_bn254(compress_proof)
+        .expect("wrap bn254 failed");
 
+    println!("Wrap proof in over groth16");
     // Wrap SNARK-friendly bn254 proof over a Groth16.
-    let artifacts_dir = get_groth16_artifacts_dir();
-    let proof = prover.wrap_groth16(outer_proof, artifacts_dir);
+    let groth16_aritfacts = try_install_groth16_artifacts();
+    let proof = prover.wrap_groth16(outer_proof, &groth16_aritfacts);
     let groth16_proof = SP1ProofWithPublicValues {
         proof,
         stdin,
