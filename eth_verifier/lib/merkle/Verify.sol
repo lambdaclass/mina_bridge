ffe3c2943cce833c130c211c31d315198231a7c8
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-06-03 14:08:32 -0300

    Add verify_path()

--- eth_verifier/lib/merkle/Verify.sol
+++ eth_verifier/lib/merkle/Verify.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.4.16 <0.9.0;
 
 import "pasta/Fields.sol";
+import "poseidon/Sponge.sol";
 
 library MerkleVerifier {
     enum LeftOrRight {
@@ -16,6 +17,29 @@ library MerkleVerifier {
 
     function verify_path(
         MerklePathElement[] memory merkle_path,
-        MerklePathElement memory leaf_hash
-    ) internal {}
+        MerklePathElement memory leaf_hash,
+        Poseidon poseidon
+    ) internal view returns (Pasta.Fp) {
+        Pasta.Fp acc = leaf_hash.hash;
+
+        for (uint256 i = 0; i < merkle_path.length; i++) {
+            Poseidon.Sponge memory sponge = poseidon.new_sponge();
+            poseidon.absorb(sponge, get_salt(i));
+
+            if (merkle_path[i].left_or_right == LeftOrRight.Left) {
+                poseidon.absorb(sponge, acc);
+                poseidon.absorb(sponge, merkle_path[i].hash);
+            } else {
+                poseidon.absorb(sponge, merkle_path[i].hash);
+                poseidon.absorb(sponge, acc);
+            }
+            (sponge, acc) = poseidon.squeeze(sponge);
+        }
+
+        return acc;
+    }
+
+    function get_salt(uint256 i) private pure returns (Pasta.Fp) {
+        return Pasta.Fp.wrap(i); // FIXME: placeholder
+    }
 }
