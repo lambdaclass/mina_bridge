e0d3040b1da1e7b8f59af737e1c8fbbaea79cd47
gabrielbosio <gabriel.bosio@lambdaclass.com>
2024-06-04 19:39:02 -0300

    Change fork branch

--- state_utility/run.sh
+++ state_utility/run.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/lambdaclass/mina.git -b 3.0.0devnet --recursive mina_3_0_0_devnet
+git clone https://github.com/lambdaclass/mina.git -b merkle_root_parser --recursive mina_3_0_0_devnet
 cd mina_3_0_0_devnet/src/lib/merkle_root_parser
 opam exec -- dune exec bin/main.exe
