4f504dd32c35e25e9bbc510b5d52e3017aafc60c
Pablo Deymonnaz <pablo.deymonnaz@lambdaclass.com>
2024-06-05 12:56:24 -0300

    param imprived in main

--- merkle_path/src/main.rs
+++ merkle_path/src/main.rs
@@ -2,7 +2,6 @@ use merkle_path::process_input_json;
 
 fn main() -> Result<(), String> {
     let args: Vec<String> = std::env::args().collect();
-
-    let path = &args[1];
+    let path = args.get(1).ok_or("Error: No input file provided")?;
     process_input_json(path, "./out.bin")
 }
