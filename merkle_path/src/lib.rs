a9efc56de5ca6f1cf66069e05a8eb92406c9bd0f
Gabriel Bosio <38794644+gabrielbosio@users.noreply.github.com>
2024-06-05 17:19:20 -0300

    Generate leaf hash and send it to Solidity (#264)
    * Initial version of merkle tree parser
    
    * query the node to get the merkle path
    
    * Fix merkle path Fp deserialization
    
    We still need to add this deserialization to the Merkle path
    serde's Deserialize trait implementation.
    
    * Add direction flag
    
    * Integrate map creation
    
    * cfg test
    
    * [WIP] Continue with hash test
    
    * Move hash logic into function
    
    * Dependencies from github address instead of local path
    
    * remove unused dependencies
    
    * Add leaf hash query
    
    * merkle path query refactored
    
    * first version of merkle root
    
    * query merkle root
    
    * Add OCaml ledger hash decoding
    
    * Make root fn return vector of bytes
    
    * [WIP] Start using openmina Merkle tree fn
    
    * [WIP] Start deserializing Mina full account
    
    * [WIP] Implement string and receipt chain hash deser
    
    * Implement account querying
    
    * impl Into<MerkleTreeNode> for MerkleLeaf
    
    * impl Into<Vec<MerkleTreeNode>> for MerkleTree
    
    * solving compilation dependencies
    
    * calc_merkle_root_hash
    
    * Warnings removed
    
    * Unnecesary code removed
    
    * warnings removed
    
    * code removed
    
    * serialize function implemented
    
    * warnings removed
    
    * dependencies removed
    
    * main program reads json file and outputs bin
    
    * Change to parse merkle root
    
    * Query merkle root
    
    * Change fork branch
    
    * Add merkle root parser
    
    * Revert merkle path component
    
    * Integrate state utility and merkle path crates
    
    * deserialization fixed
    
    * ignore out.bin
    
    * ignore target/
    
    * clippy
    
    * clippy 2
    
    * fmt
    
    * Add merkle root serialization
    
    * separation of main function in a lib.rs file
    
    * integration test
    
    * merkle_path directory renamed and CI tests
    
    * clippy
    
    * clippy in tests
    
    * Skip run for merkle path crate
    
    * unwraps removed and doc added
    
    * fmt
    
    * account.rs removed
    
    * Make state utility use new merkle path crate
    
    * unused dependency removed
    
    * param imprived in main
    
    * extend() used
    
    * Add tests
    
    * input path improved
    
    * Add CI job
    
    * variable renamed
    
    * Ignore test output file
    
    * Refactor match in str conversion
    
    * Remove unused JSON
    
    * cargo command with manifest-path
    
    * Remove diff markers
    
    * clippy
    
    * show error in parse_merkle_root
    
    * query_merkle_root fixed
    
    * query_merkle_root fixed in merkle_root.rs
    
    * query_merkle_root improved
    
    * clippy warnings
    
    * Send leaf hash to Solidity
    
    * Rearrange returned values
    
    ---------
    
    Co-authored-by: Pablo Deymonnaz <deymonnaz@gmail.com>
    Co-authored-by: Pablo Deymonnaz <pablo.deymonnaz@lambdaclass.com>
    Co-authored-by: gabrielbosio <gabriel.bosio@lambdaclass.com>

--- merkle_path/src/lib.rs
+++ merkle_path/src/lib.rs
@@ -22,23 +22,25 @@ use std::io::Write;
 /// the content cannot be deserialized to JSON,
 /// or the output file cannot be created or written to.
 pub fn process_input_json(
-    merkle_tree_path: &str,
+    public_key_path: &str,
     merkle_root_path: &str,
     output_path: &str,
 ) -> Result<(), String> {
-    let merkle_tree_content = std::fs::read_to_string(merkle_tree_path)
+    let public_key = std::fs::read_to_string(public_key_path)
         .map_err(|err| format!("Error opening file {err}"))?;
-    let merkle_tree: MerkleTree = serde_json::from_str(&merkle_tree_content)
-        .map_err(|err| format!("Error deserializing content to JSON {err}"))?;
+    let merkle_tree = MerkleTree::query_merkle_path(&public_key)?;
 
     let merkle_root_content = std::fs::read_to_string(merkle_root_path)
         .map_err(|err| format!("Error opening file {err}"))?;
     let merkle_root: Fp = field::from_str(&merkle_root_content)
-        .map_err(|err| format!("Error deserializing content to JSON {err}"))?;
+        .map_err(|err| format!("Error deserializing Merkle root to field {err}"))?;
+    let leaf_hash = field::from_str(&merkle_tree.data.account.leaf_hash)
+        .map_err(|err| format!("Error deserializing leaf hash to field {err}"))?;
 
     let ret_to_bytes = [
-        merkle_tree.data.account.merkle_path.to_bytes(),
         field::to_bytes(&merkle_root)?,
+        field::to_bytes(&leaf_hash)?,
+        merkle_tree.data.account.merkle_path.to_bytes(),
     ]
     .concat();
 
--- merkle_path/src/main.rs
+++ merkle_path/src/main.rs
@@ -3,7 +3,7 @@ use merkle_path::process_input_json;
 fn main() -> Result<(), String> {
     let mut args = std::env::args();
 
-    let merkle_tree_path = args.nth(1).ok_or("Error: No merkle tree file provided")?;
+    let public_key_path = args.nth(1).ok_or("Error: No public key file provided")?;
     let merkle_root_path = args.nth(2).ok_or("Error: No merkle root file provided")?;
-    process_input_json(&merkle_tree_path, &merkle_root_path, "./out.bin")
+    process_input_json(&public_key_path, &merkle_root_path, "./out.bin")
 }
