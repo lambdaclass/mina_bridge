0e75ff0bb98cc1b42b377a0f21e854f02b1dd6fd
gabrielbosio <gabrielbosio95@gmail.com>
2024-06-05 19:55:43 -0300

    Fix clippy

--- merkle_path/src/main.rs
+++ merkle_path/src/main.rs
@@ -4,5 +4,5 @@ fn main() -> Result<(), String> {
     let args: Vec<String> = std::env::args().collect();
 
     let public_key_path = args.get(1).ok_or("Error: No public key file provided")?;
-    process_input_json(&public_key_path, "./leaf_hash.bin", "./merkle_path.bin")
+    process_input_json(public_key_path, "./leaf_hash.bin", "./merkle_path.bin")
 }
