f7a41294b059d29f49293f357bdbc3a0b1f98661
gabrielbosio <gabrielbosio95@gmail.com>
2024-06-05 19:08:53 -0300

    Parameterize output file in parser

--- state_utility/parser/src/main.rs
+++ state_utility/parser/src/main.rs
@@ -1,5 +1,8 @@
 use merkle_root_parser::parse_merkle_root;
 
-fn main() {
-    parse_merkle_root("merkle_root.bin").unwrap();
+fn main() -> Result<(), String> {
+    let args: Vec<String> = std::env::args().collect();
+
+    let merkle_root_out_path = args.get(1).ok_or("Error: No merkle root file provided")?;
+    parse_merkle_root(merkle_root_out_path)
 }
