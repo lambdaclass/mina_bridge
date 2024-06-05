1e8cbd963c9659c6d1b8054f35e81dfe694fe27f
Pablo Deymonnaz <pablo.deymonnaz@lambdaclass.com>
2024-06-05 18:18:50 -0300

    fix params

--- state_utility/merkle_root/src/main.rs
+++ state_utility/merkle_root/src/main.rs
@@ -1,9 +1,9 @@
 use merkle_root::process_merkle_root;
 
 fn main() -> Result<(), String> {
-    let mut args = std::env::args();
+    let args: Vec<String> = std::env::args().collect();
 
-    let merkle_root_in_path = args.nth(1).ok_or("Error: No merkle root file provided")?;
-    let merkle_root_out_path = args.nth(2).ok_or("Error: No merkle root out path")?;
-    process_merkle_root(&merkle_root_in_path, &merkle_root_out_path)
+    let merkle_root_in_path = args.get(1).ok_or("Error: No merkle root file provided")?;
+    let merkle_root_out_path = args.get(2).ok_or("Error: No merkle root out path")?;
+    process_merkle_root(merkle_root_in_path, merkle_root_out_path)
 }
