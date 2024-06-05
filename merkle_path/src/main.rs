1b843932ad0f9f5227e085ba5aa2e55643d6039d
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-06-05 17:55:24 -0300

    Fix typo

--- merkle_path/src/main.rs
+++ merkle_path/src/main.rs
@@ -4,6 +4,6 @@ fn main() -> Result<(), String> {
     let args: Vec<String> = std::env::args().collect();
 
     let public_key_path = args.get(1).ok_or("Error: No public key file provided")?;
-    let merkle_root_path = args.get(1).ok_or("Error: No merkle root file provided")?;
+    let merkle_root_path = args.get(2).ok_or("Error: No merkle root file provided")?;
     process_input_json(public_key_path, merkle_root_path, "./out.bin")
 }
