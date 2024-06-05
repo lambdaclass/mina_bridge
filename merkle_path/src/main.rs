8e34a108e5acbb48db6e66ac6342c3ca11288832
Pablo Deymonnaz <pablo.deymonnaz@lambdaclass.com>
2024-06-05 13:02:45 -0300

    variable renamed

--- merkle_path/src/main.rs
+++ merkle_path/src/main.rs
@@ -1,8 +1,8 @@
 use merkle_path::process_input_json;
 
 fn main() -> Result<(), String> {
-    let path = std::env::args()
+    let input_path = std::env::args()
         .nth(1)
         .ok_or("Error: No input file provided")?;
-    process_input_json(&path, "./out.bin")
+    process_input_json(&input_path, "./out.bin")
 }
