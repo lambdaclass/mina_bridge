a13536f7707796ab0af124911e72b0b348d8ef06
Pablo Deymonnaz <pablo.deymonnaz@lambdaclass.com>
2024-06-05 17:28:15 -0300

    parsing args modified

--- merkle_path/src/main.rs
+++ merkle_path/src/main.rs
@@ -1,9 +1,9 @@
 use merkle_path::process_input_json;
 
 fn main() -> Result<(), String> {
-    let mut args = std::env::args();
+    let args: Vec<String> = std::env::args().collect();
 
-    let merkle_tree_path = args.nth(1).ok_or("Error: No merkle tree file provided")?;
-    let merkle_root_path = args.nth(2).ok_or("Error: No merkle root file provided")?;
+    let merkle_tree_path = args.get(1).ok_or("Error: No merkle tree file provided")?;
+    let merkle_root_path = args.get(2).ok_or("Error: No merkle root file provided")?;
     process_input_json(&merkle_tree_path, &merkle_root_path, "./out.bin")
 }
