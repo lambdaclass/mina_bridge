bbc055bad1916ac321a2097ff3192a218ab65a43
gabrielbosio <gabrielbosio95@gmail.com>
2024-06-05 12:49:40 -0300

    Make state utility use new merkle path crate

--- state_utility/run.sh
+++ state_utility/run.sh
@@ -3,4 +3,4 @@ cargo r --release
 cd ..
 git clone https://github.com/lambdaclass/mina.git -b merkle_root_parser --recursive mina_3_0_0_devnet
 cd mina_3_0_0_devnet/src/lib/merkle_root_parser
-opam exec -- dune exec bin/main.exe > ../../../../../merkle-path/merkle_root.txt
+opam exec -- dune exec bin/main.exe > ../../../../../merkle_path/merkle_root.txt
