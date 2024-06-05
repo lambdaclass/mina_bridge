0612af5219e6636c7f90bd3272b4039603a212e0
Pablo Deymonnaz <pablo.deymonnaz@lambdaclass.com>
2024-06-05 13:01:52 -0300

    input path improved

--- merkle_path/src/main.rs
+++ merkle_path/src/main.rs
@@ -1,7 +1,8 @@
 use merkle_path::process_input_json;
 
 fn main() -> Result<(), String> {
-    let args: Vec<String> = std::env::args().collect();
-    let path = args.get(1).ok_or("Error: No input file provided")?;
-    process_input_json(path, "./out.bin")
+    let path = std::env::args()
+        .nth(1)
+        .ok_or("Error: No input file provided")?;
+    process_input_json(&path, "./out.bin")
 }
