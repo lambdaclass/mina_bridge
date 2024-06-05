2a15910c0b7495460492019540fc708d966fef13
Pablo Deymonnaz <pablo.deymonnaz@lambdaclass.com>
2024-06-05 13:26:01 -0300

    cargo command with manifest-path

--- state_utility/run.sh
+++ state_utility/run.sh
@@ -2,8 +2,10 @@
 
 git clone https://github.com/lambdaclass/mina.git -b merkle_root_parser --recursive mina_3_0_0_devnet
 
-cd parser
-cargo r --release -- ../mina_3_0_0_devnet/src/lib/merkle_root_parser/merkle_root.txt
+cargo r \
+    --manifest-path parser/\
+    --release \
+    -- ./mina_3_0_0_devnet/src/lib/merkle_root_parser/merkle_root.txt
 
-cd ../mina_3_0_0_devnet/src/lib/merkle_root_parser
+cd ./mina_3_0_0_devnet/src/lib/merkle_root_parser
 opam exec -- dune exec bin/main.exe > ../../../../../merkle_path/merkle_root.txt
