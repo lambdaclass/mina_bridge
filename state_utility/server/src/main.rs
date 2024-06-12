6b39da7eb110c46d9fe54cb6e05dbd8d55e751c8
gabrielbosio <gabrielbosio95@gmail.com>
2024-06-12 17:56:28 -0300

    Add database query

--- state_utility/server/src/main.rs
+++ state_utility/server/src/main.rs
@@ -1,18 +1,23 @@
-use axum::{extract::Path, routing::get, Router};
-use sqlx::postgres::PgPoolOptions;
+use axum::{
+    extract::{Path, State},
+    routing::get,
+    Json, Router,
+};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect("postgres://postgres:password@localhost/test")
+        .connect("postgres://pguser:pguser@localhost/archive")
         .await
         .map_err(|err| format!("Could not create a connection pool for Postgres: {err}"))?;
 
     // build our application with a route
     let app = Router::new()
         .route("/", get(root))
-        .route("/balance/:public_key", get(balance));
+        .route("/balance/:public_key", get(balance))
+        .with_state(pool);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -26,6 +31,16 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
-async fn balance(Path(public_key): Path<String>) -> String {
-    public_key
+async fn balance(
+    State(pool): State<Pool<Postgres>>,
+    Path(public_key): Path<String>,
+) -> Result<Json<String>, String> {
+    let row: (String,) = sqlx::query_as(&format!(
+        "SELECT value FROM public_keys WHERE value = '{public_key}';"
+    ))
+    .fetch_one(&pool)
+    .await
+    .map_err(|err| format!("Could not query the database: {err}"))?;
+
+    Ok(Json(row.0))
 }
