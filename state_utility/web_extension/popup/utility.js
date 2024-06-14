a8d36fdd5a75d26fe51b3c1243cba68bd97c1c41
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-06-14 17:28:13 -0300

    Fetch

--- state_utility/web_extension/popup/utility.js
+++ state_utility/web_extension/popup/utility.js
@@ -3,15 +3,16 @@ function accountForm() {
 
     const button = document.getElementById("submit");
 
-    button.addEventListener("click", (event) => {
-        const verified = document.getElementById("verified");
-        // query server, check account
-        // ...
-        // here we put the code!
-        let success = true;
-        let balance = 10;
+    button.addEventListener("click", async (event) => {
+        //const publicKey = document.getElementById("public-key").nodeValue;
+        const publicKey = "B62qpWKzx7e1mmwVf8dJAPFQPGVpZP9pJaobhvg8iagqU2r1bEyndMa";
+        //const response = await fetch("http://5.9.57.89:3000/account_state/".concat(publicKey, "/http%3A%2F%2F5.9.57.89%3A3085%2Fgraphql/https%3A%2F%2Fsepolia.drpc.org/0x698176F675F7e05a751c70bC902Ec18c5D228b8E"));
+        //const valid = response.valid
+        //const balance = response.balance
+        const valid = true;
+        const balance = 100;
 
-        if (success) {
+        if (valid) {
             document.getElementById("verified").textContent = "yes!";
             document.getElementById("balance").textContent = balance;
         }
