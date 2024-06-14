b96289ddb50e0fdc0ce118f314a1bb0ff9bb960c
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-06-14 16:42:58 -0300

    button click fixed

--- state_utility/web_extension/popup/utility.js
+++ state_utility/web_extension/popup/utility.js
@@ -1,13 +1,21 @@
 function accountForm() {
     //let publicKey = document.forms["account-form"]["public-key"];
 
-    // query server, check account
-    // ...
-    let success = true;
-    let balance = 10;
+    const button = document.getElementById("submit");
 
-    if (success) {
-        document.getElementById("verified").textContent = "yes!";
-        document.getElementById("balance").textContent = balance;
-    }
+    button.addEventListener("click", (event) => {
+        const verified = document.getElementById("verified");
+        // query server, check account
+        // ...
+        // here we put the code!
+        let success = true;
+        let balance = 10;
+
+        if (success) {
+            document.getElementById("verified").textContent = "yes!";
+            document.getElementById("balance").textContent = balance;
+        }
+    });
 }
+
+window.onload = accountForm;
