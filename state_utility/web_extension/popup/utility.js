51eede6b85758ef9ddb5545bfc7597862a79b445
gabrielbosio <gabrielbosio95@gmail.com>
2024-06-14 18:39:13 -0300

    [WIP] Integrate with state utility server

--- state_utility/web_extension/popup/utility.js
+++ state_utility/web_extension/popup/utility.js
@@ -4,13 +4,12 @@ function accountForm() {
     const button = document.getElementById("submit");
 
     button.addEventListener("click", async (event) => {
-        //const publicKey = document.getElementById("public-key").nodeValue;
-        const publicKey = "B62qpWKzx7e1mmwVf8dJAPFQPGVpZP9pJaobhvg8iagqU2r1bEyndMa";
-        //const response = await fetch("http://5.9.57.89:3000/account_state/".concat(publicKey, "/http%3A%2F%2F5.9.57.89%3A3085%2Fgraphql/https%3A%2F%2Fsepolia.drpc.org/0x698176F675F7e05a751c70bC902Ec18c5D228b8E"));
-        //const valid = response.valid
-        //const balance = response.balance
-        const valid = true;
-        const balance = 100;
+        const publicKey = document.getElementById("public-key").value;
+        const response = fetch("http://mina-builder:3000/account_state/".concat(publicKey, "/http%3A%2F%2F5.9.57.89%3A3085%2Fgraphql/https%3A%2F%2Fsepolia.drpc.org/0x698176F675F7e05a751c70bC902Ec18c5D228b8E"))
+            .then(() => { alert("http://5.9.57.89:3000/account_state/".concat(publicKey, "/http%3A%2F%2F5.9.57.89%3A3085%2Fgraphql/https%3A%2F%2Fsepolia.drpc.org/0x698176F675F7e05a751c70bC902Ec18c5D228b8E")); })
+            .catch((err) => { document.getElementById("verified").textContent = err; });
+        const valid = response.valid
+        const balance = response.balance
 
         if (valid) {
             document.getElementById("verified").textContent = "yes!";
