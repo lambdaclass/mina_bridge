1fd35af99286ed38063582f9b5432e71488c2666
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-06-17 17:36:55 -0300

    Added styling with tailwind

--- state_utility/web_extension/popup/utility.js
+++ state_utility/web_extension/popup/utility.js
@@ -5,15 +5,28 @@ function accountForm() {
 
     button.addEventListener("click", async (event) => {
         const publicKey = document.getElementById("public-key").value;
-        const response = fetch("http://mina-builder:3000/account_state/".concat(publicKey, "/http%3A%2F%2F5.9.57.89%3A3085%2Fgraphql/https%3A%2F%2Fsepolia.drpc.org/0x698176F675F7e05a751c70bC902Ec18c5D228b8E"))
-            .then(() => { alert("http://5.9.57.89:3000/account_state/".concat(publicKey, "/http%3A%2F%2F5.9.57.89%3A3085%2Fgraphql/https%3A%2F%2Fsepolia.drpc.org/0x698176F675F7e05a751c70bC902Ec18c5D228b8E")); })
-            .catch((err) => { document.getElementById("verified").textContent = err; });
-        const valid = response.valid
-        const balance = response.balance
+        //const response = fetch("http://mina-builder:3000/account_state/".concat(publicKey, "/http%3A%2F%2F5.9.57.89%3A3085%2Fgraphql/https%3A%2F%2Fsepolia.drpc.org/0x698176F675F7e05a751c70bC902Ec18c5D228b8E"))
+        //    .then(() => { alert("http://5.9.57.89:3000/account_state/".concat(publicKey, "/http%3A%2F%2F5.9.57.89%3A3085%2Fgraphql/https%3A%2F%2Fsepolia.drpc.org/0x698176F675F7e05a751c70bC902Ec18c5D228b8E")); })
+        //    .catch((err) => { document.getElementById("error").textContent = err; });
+        //let valid = response.valid
+        //let balance = response.balance
+
+        let valid = false;
+        let balance = 0;
+
+        if (publicKey === "verified") {
+            valid = true;
+            balance = 87.4
+        }
 
         if (valid) {
-            document.getElementById("verified").textContent = "yes!";
             document.getElementById("balance").textContent = balance;
+            document.getElementById("verified").classList.remove("hidden");
+            document.getElementById("notverified").classList.add("hidden");
+        } else {
+            document.getElementById("balance").textContent = balance;
+            document.getElementById("verified").classList.add("hidden");
+            document.getElementById("notverified").classList.remove("hidden");
         }
     });
 }
---
+++ state_utility/web_extension/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./popup/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
