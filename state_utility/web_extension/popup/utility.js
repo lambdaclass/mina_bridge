6b69f788758f89858048964798d7ce380aa3a51f
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-06-18 16:34:11 -0300

    Beautify, fix balance

--- state_utility/web_extension/popup/utility.js
+++ state_utility/web_extension/popup/utility.js
@@ -3,9 +3,28 @@ function accountForm() {
 
     const button = document.getElementById("submit");
 
-        async function request() {
-            const publicKey = document.getElementById("public-key").value;
-            const string = "http://5.9.57.89:3000/account_state/".concat(publicKey, "/http%3A%2F%2F5.9.57.89%3A3085%2Fgraphql/https%3A%2F%2Fsepolia.drpc.org/0x698176F675F7e05a751c70bC902Ec18c5D228b8E");
+    const show_valid_account = (balance) => {
+        document.getElementById("balance").textContent = balance * (10**-9);
+        document.getElementById("verified").classList.remove("hidden");
+        document.getElementById("notverified").classList.add("hidden");
+    }
+
+    const show_invalid_account = () => {
+        document.getElementById("verified").classList.add("hidden");
+        document.getElementById("notverified").classList.remove("hidden");
+        document.getElementById("errormsg").classList.add("hidden");
+    }
+
+    const show_error = () => {
+        document.getElementById("balance").textContent = "?";
+        document.getElementById("verified").classList.add("hidden");
+        document.getElementById("notverified").classList.add("hidden");
+        document.getElementById("errormsg").classList.remove("hidden");
+    }
+
+    async function request() {
+        const publicKey = document.getElementById("public-key").value;
+        const string = "http://5.9.57.89:3000/account_state/".concat(publicKey, "/http%3A%2F%2F5.9.57.89%3A3085%2Fgraphql/https%3A%2F%2Fsepolia.drpc.org/0x698176F675F7e05a751c70bC902Ec18c5D228b8E");
         try {
             const response = await fetch(string);
             try {
@@ -13,24 +32,15 @@ function accountForm() {
                 let valid = response_json.valid
                 if (valid) {
                     let balance = response_json.balance
-                    document.getElementById("balance").textContent = balance;
-                    document.getElementById("verified").classList.remove("hidden");
-                    document.getElementById("notverified").classList.add("hidden");
+                    show_valid_account(balance)
                 } else {
-                    document.getElementById("verified").classList.add("hidden");
-                    document.getElementById("notverified").classList.remove("hidden");
+                    show_invalid_account();
                 }
             } catch {
-                document.getElementById("balance").textContent = "?";
-                document.getElementById("verified").classList.add("hidden");
-                document.getElementById("notverified").classList.add("hidden");
-                document.getElementById("errormsg").classList.remove("hidden");
+                show_error();
             }
         } catch {
-            document.getElementById("balance").textContent = "?";
-            document.getElementById("verified").classList.add("hidden");
-            document.getElementById("notverified").classList.add("hidden");
-            document.getElementById("errormsg").classList.remove("hidden");
+            show_error();
         }
     }
 
