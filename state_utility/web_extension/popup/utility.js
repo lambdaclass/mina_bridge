7d338d7c109d1f88ca7c1c8aa99757a706725eb0
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-06-18 16:27:41 -0300

    Fetch server

--- state_utility/web_extension/popup/utility.js
+++ state_utility/web_extension/popup/utility.js
@@ -3,32 +3,38 @@ function accountForm() {
 
     const button = document.getElementById("submit");
 
-    button.addEventListener("click", async (event) => {
-        const publicKey = document.getElementById("public-key").value;
-        //const response = fetch("http://mina-builder:3000/account_state/".concat(publicKey, "/http%3A%2F%2F5.9.57.89%3A3085%2Fgraphql/https%3A%2F%2Fsepolia.drpc.org/0x698176F675F7e05a751c70bC902Ec18c5D228b8E"))
-        //    .then(() => { alert("http://5.9.57.89:3000/account_state/".concat(publicKey, "/http%3A%2F%2F5.9.57.89%3A3085%2Fgraphql/https%3A%2F%2Fsepolia.drpc.org/0x698176F675F7e05a751c70bC902Ec18c5D228b8E")); })
-        //    .catch((err) => { document.getElementById("error").textContent = err; });
-        //let valid = response.valid
-        //let balance = response.balance
-
-        let valid = false;
-        let balance = "?";
-
-        if (publicKey === "verified") {
-            valid = true;
-            balance = 87.4
-        }
-
-        if (valid) {
-            document.getElementById("balance").textContent = balance;
-            document.getElementById("verified").classList.remove("hidden");
-            document.getElementById("notverified").classList.add("hidden");
-        } else {
-            document.getElementById("balance").textContent = balance;
+        async function request() {
+            const publicKey = document.getElementById("public-key").value;
+            const string = "http://5.9.57.89:3000/account_state/".concat(publicKey, "/http%3A%2F%2F5.9.57.89%3A3085%2Fgraphql/https%3A%2F%2Fsepolia.drpc.org/0x698176F675F7e05a751c70bC902Ec18c5D228b8E");
+        try {
+            const response = await fetch(string);
+            try {
+                const response_json = await response.json();
+                let valid = response_json.valid
+                if (valid) {
+                    let balance = response_json.balance
+                    document.getElementById("balance").textContent = balance;
+                    document.getElementById("verified").classList.remove("hidden");
+                    document.getElementById("notverified").classList.add("hidden");
+                } else {
+                    document.getElementById("verified").classList.add("hidden");
+                    document.getElementById("notverified").classList.remove("hidden");
+                }
+            } catch {
+                document.getElementById("balance").textContent = "?";
+                document.getElementById("verified").classList.add("hidden");
+                document.getElementById("notverified").classList.add("hidden");
+                document.getElementById("errormsg").classList.remove("hidden");
+            }
+        } catch {
+            document.getElementById("balance").textContent = "?";
             document.getElementById("verified").classList.add("hidden");
-            document.getElementById("notverified").classList.remove("hidden");
+            document.getElementById("notverified").classList.add("hidden");
+            document.getElementById("errormsg").classList.remove("hidden");
         }
-    });
+    }
+
+    button.addEventListener("click", request);
 }
 
 window.onload = accountForm;
