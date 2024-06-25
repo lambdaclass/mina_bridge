83f472f4110021d8102b209ae5eac46b2e0e67bc
gabrielbosio <gabrielbosio95@gmail.com>
2024-06-25 19:06:29 -0300

    Start using openmina (temporarily)

--- pickles/src/verifier.rs
+++ pickles/src/verifier.rs
@@ -1,20 +1,3 @@
-use kimchi::{
-    groupmap::GroupMap,
-    mina_curves::pasta::{Fp, Fq, Pallas, PallasParameters},
-    mina_poseidon::{
-        constants::PlonkSpongeConstantsKimchi,
-        sponge::{DefaultFqSponge, DefaultFrSponge},
-    },
-    verifier::batch_verify,
-};
-
-// pub fn verify() -> Result<(), String> {
-//     let group_map = GroupMap::<Fp>::setup();
-
-//     batch_verify::<
-//         Pallas,
-//         DefaultFqSponge<PallasParameters, PlonkSpongeConstantsKimchi>,
-//         DefaultFrSponge<Fq, PlonkSpongeConstantsKimchi>,
-//     >(group_map, proofs)
-//     .map_err(|err| format!("Could not verify Kimchi proof: {err}"))
-// }
+pub fn verify() -> Result<(), String> {
+    Ok(())
+}
