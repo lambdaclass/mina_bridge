8310153d838407f1a68c5629cb37989041141bf8
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-06-26 13:04:03 -0300

    Change some names and format

--- pickles/src/lib.rs
+++ pickles/src/lib.rs
@@ -7,18 +7,17 @@ pub mod state_proof;
 pub mod verifier;
 
 pub fn parse_and_verify(proof_file_path: &str) -> Result<(), String> {
-    let proof_json = fs::read_to_string(proof_file_path)
+    let proof_query_str = fs::read_to_string(proof_file_path)
         .map_err(|err| format!("Could not read proof file: {err}"))?;
-
-    let proof_query: serde_json::Map<String, serde_json::Value> = serde_json::from_str(&proof_json)
+    let proof_query: serde_json::Map<String, serde_json::Value> = serde_json::from_str(&proof_query_str)
         .map_err(|err| format!("Could not parse proof file as JSON: {err}"))?;
-    let protocol_state_proof = proof_query
+    let protocol_state_proof_json = proof_query
         .get("data")
         .and_then(|d| d.get("bestChain"))
         .and_then(|d| d.get(0))
         .and_then(|d| d.get("protocolStateProof"))
         .and_then(|d| d.get("json")).ok_or("Could not parse protocol state proof: JSON structure upto protocolStateProof is unexpected")?;
-    let proof = parse(protocol_state_proof)?;
+    let protocol_state_proof = parse(protocol_state_proof_json)?;
 
-    Ok(())
+    todo!()
 }
