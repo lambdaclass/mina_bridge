6069d3fca423db1d5ce4721f8b036a6e2d0b34dc
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-07-17 18:02:40 -0300

    Contract baseline

--- contract/src/Counter.sol
+++ contract/src/Counter.sol
@@ -1,14 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+error NewStateIsNotValid();
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+/// @title Mina to Ethereum Bridge's smart contract.
+contract MinaBridge {
+    /// @notice The state hash of the last verified state converted into a Fp.
+    uint256 public lastVerifiedStateHash;
+
+    function getLastVerifiedStateHash() public view returns (uint256) {
+        return lastVerifiedStateHash;
     }
 
-    function increment() public {
-        number++;
+    function updateLastVerifiedState() external {
+        /*
+         * parameters:
+         * - newStateVerificationData (excludes pubInputCommitment)
+         * - newState
+         * - newStateHash
+         * - lastVerifiedState
+         *
+         * pseudocode:
+         *    pubInputs = [newState, newStateHash, lastVerifiedState, lastVerifiedStateHash]
+         *    pubInputsCommitment = keccak256(pubInputs)
+         *
+         *    bool isNewStateVerified = AlignedLayerServiceManager.verifyBatchInclusion(
+         *        newStateVerificationData, pubInputCommitment
+         *    );
+         *
+         *    if (isNewStateVerified) {
+         *        lastVerifiedStateHash = newStateHash;
+         *    } else {
+         *        revert NewStateIsNotValid();
+         *    }
+         */
     }
 }
