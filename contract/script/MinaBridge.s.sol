19cfda44e088c37176d88496960c4f933e57b5b0
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-07-17 18:06:01 -0300

    Rename files and contract

--- contract/script/MinaBridge.s.sol
+++ contract/script/MinaBridge.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {MinaBridge} from "../src/MinaBridge.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract MinaBridgeScript is Script {
+    MinaBridge public bridge;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        bridge = new MinaBridge();
 
         vm.stopBroadcast();
     }
---
+++ contract/test/MinaBridge.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MinaBridge} from "../src/MinaBridge.sol";
+
+contract MinaBridgeTest is Test {
+    MinaBridge public bridge;
+
+    function setUp() public {
+        counter = new MinaBridge();
+    }
+}
