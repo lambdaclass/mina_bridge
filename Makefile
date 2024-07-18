941310f5e016064f4945bf2edbc332a17116c4ec
gabrielbosio <gabrielbosio95@gmail.com>
2024-07-18 18:58:38 -0300

    Simplify and add make cmd

--- Makefile
+++ Makefile
@@ -12,3 +12,14 @@ run:
 		--proof_generator_addr 0x66f9664f97F2b50F62D13eA064982f936dE76657
 	@echo "Done!"
 
+verify_account:
+	@echo "Generating proof of account inclusion..."
+	@cargo run --manifest-path account_inclusion/script/Cargo.toml --release
+	@echo "Done!"
+	@echo "Sending Account inclusion proof to Aligned..."
+	@cargo run --manifest-path ${ALIGNED_PATH}/batcher/aligned/Cargo.toml --release -- submit \
+		--proving_system SP1 \
+		--proof account_inclusion.proof \
+		--vm_program account_inclusion/program/elf/riscv32im-succinct-zkvm-elf \
+		--proof_generator_addr 0x66f9664f97F2b50F62D13eA064982f936dE76657
+	@echo "Done!"
--- account_inclusion/script/src/bin/prove.rs
+++ account_inclusion/script/src/bin/prove.rs
@@ -8,10 +8,8 @@
 
 use std::path::PathBuf;
 
-use alloy_sol_types::{sol, SolType};
 use clap::Parser;
-use serde::{Deserialize, Serialize};
-use sp1_sdk::{HashableKey, ProverClient, SP1PlonkBn254Proof, SP1Stdin, SP1VerifyingKey};
+use sp1_sdk::{ProverClient, SP1Stdin};
 
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
 ///
@@ -24,15 +22,7 @@ pub const FIBONACCI_ELF: &[u8] = include_bytes!("../../../program/elf/riscv32im-
 struct ProveArgs {
     #[clap(long, default_value = "20")]
     n: u32,
-
-    #[clap(long, default_value = "false")]
-    evm: bool,
 }
-
-/// The public values encoded as a tuple that can be easily deserialized inside Solidity.
-type PublicValuesTuple = sol! {
-    tuple(uint32, uint32, uint32)
-};
 
 fn main() {
     // Setup the logger.
@@ -53,75 +43,15 @@ fn main() {
 
     println!("n: {}", args.n);
 
-    if args.evm {
-        // Generate the proof.
-        let proof = client
-            .prove_plonk(&pk, stdin)
-            .expect("failed to generate proof");
-        create_plonk_fixture(&proof, &vk);
-    } else {
-        // Generate the proof.
-        let proof = client.prove(&pk, stdin).expect("failed to generate proof");
-        let (_, _, fib_n) =
-            PublicValuesTuple::abi_decode(proof.public_values.as_slice(), false).unwrap();
-        println!("Successfully generated proof!");
-        println!("fib(n): {}", fib_n);
+    // Generate the proof.
+    let proof = client
+        .prove_compressed(&pk, stdin)
+        .expect("failed to generate proof");
 
-        // Verify the proof.
-        client.verify(&proof, &vk).expect("failed to verify proof");
-    }
-}
-
-/// A fixture that can be used to test the verification of SP1 zkVM proofs inside Solidity.
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct SP1FibonacciProofFixture {
-    a: u32,
-    b: u32,
-    n: u32,
-    vkey: String,
-    public_values: String,
-    proof: String,
-}
-
-/// Create a fixture for the given proof.
-fn create_plonk_fixture(proof: &SP1PlonkBn254Proof, vk: &SP1VerifyingKey) {
-    // Deserialize the public values.
-    let bytes = proof.public_values.as_slice();
-    let (n, a, b) = PublicValuesTuple::abi_decode(bytes, false).unwrap();
-
-    // Create the testing fixture so we can test things end-ot-end.
-    let fixture = SP1FibonacciProofFixture {
-        a,
-        b,
-        n,
-        vkey: vk.bytes32().to_string(),
-        public_values: proof.public_values.bytes().to_string(),
-        proof: proof.bytes().to_string(),
-    };
-
-    // The verification key is used to verify that the proof corresponds to the execution of the
-    // program on the given input.
-    //
-    // Note that the verification key stays the same regardless of the input.
-    println!("Verification Key: {}", fixture.vkey);
-
-    // The public values are the values whicha are publically commited to by the zkVM.
-    //
-    // If you need to expose the inputs or outputs of your program, you should commit them in
-    // the public values.
-    println!("Public Values: {}", fixture.public_values);
-
-    // The proof proves to the verifier that the program was executed with some inputs that led to
-    // the give public values.
-    println!("Proof Bytes: {}", fixture.proof);
-
-    // Save the fixture to a file.
-    let fixture_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../contracts/src/fixtures");
-    std::fs::create_dir_all(&fixture_path).expect("failed to create fixture path");
-    std::fs::write(
-        fixture_path.join("fixture.json"),
-        serde_json::to_string_pretty(&fixture).unwrap(),
-    )
-    .expect("failed to write fixture");
+    let mut proof_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    proof_path.push("../../account_inclusion.proof");
+    proof.save(proof_path).expect("failed to save proof");
+    client
+        .verify_compressed(&proof, &vk)
+        .expect("Verification error");
 }
