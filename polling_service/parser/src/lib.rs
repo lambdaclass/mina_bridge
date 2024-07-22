c16e4c656d528370b6d04a70a04476519a502521
gabrielbosio <gabrielbosio95@gmail.com>
2024-07-22 18:23:03 -0300

    Read protocol state proof

--- polling_service/parser/src/lib.rs
+++ polling_service/parser/src/lib.rs
@@ -4,13 +4,22 @@ use kimchi::o1_utils::FieldHelpers;
 use mina_curves::pasta::Fp;
 use reqwest::header::CONTENT_TYPE;
 
-pub fn parse_public_input(rpc_url: &str, output_path: &str) -> Result<(), String> {
+pub fn parse_public_input(
+    rpc_url: &str,
+    proof_path: &str,
+    public_input_path: &str,
+) -> Result<(), String> {
     let response_value = query_to_mina_node(rpc_url)?;
+
+    let proof = serialize_protocol_state_proof(&response_value)?;
+
     let mut public_input = serialize_state_hash_field(&response_value)?;
     public_input.extend(serialize_protocol_state(&response_value)?);
 
-    fs::write(output_path, public_input)
-        .map_err(|err| format!("Error writing state proof to file: {err}"))
+    fs::write(proof_path, proof)
+        .map_err(|err| format!("Error writing state proof to file: {err}"))?;
+    fs::write(public_input_path, public_input)
+        .map_err(|err| format!("Error writing public input to file: {err}"))
 }
 
 fn query_to_mina_node(rpc_url: &str) -> Result<serde_json::Value, String> {
@@ -18,6 +27,9 @@ fn query_to_mina_node(rpc_url: &str) -> Result<serde_json::Value, String> {
             protocolState(encoding: BASE64)
             bestChain(maxLength: 1) {
                 stateHashField
+                protocolStateProof {
+                    base64
+                }
             }
         }\"}"
         .to_owned();
@@ -80,6 +92,26 @@ fn serialize_protocol_state(response_value: &serde_json::Value) -> Result<Vec<u8
     Ok(protocol_state_bytes)
 }
 
+fn serialize_protocol_state_proof(response_value: &serde_json::Value) -> Result<Vec<u8>, String> {
+    let protocol_state_proof_str = response_value
+        .get("bestChain")
+        .and_then(|d| d.get(0))
+        .and_then(|d| d.get("protocolStateProof"))
+        .and_then(|d| d.get("base64"))
+        .ok_or(format!(
+            "Error getting 'bestChain[0].protocolStateProof.base64' from {:?}",
+            response_value
+        ))?
+        .as_str()
+        .ok_or(format!(
+            "Error converting protocol state proof value to string: {:?}",
+            response_value,
+        ))?;
+    let protocol_state_proof_bytes = protocol_state_proof_str.as_bytes().to_vec();
+
+    Ok(protocol_state_proof_bytes)
+}
+
 #[cfg(test)]
 mod tests {
     use std::path::PathBuf;
@@ -88,12 +120,15 @@ mod tests {
 
     #[test]
     fn serialize_and_deserialize() {
-        let mut state_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        state_path.push("../../protocol_state.pub");
+        let mut proof_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        proof_path.push("../../protocol_state.proof");
+        let mut public_input_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        public_input_path.push("../../protocol_state.pub");
 
         parse_public_input(
             "http://localhost:3085/graphql",
-            state_path.to_str().unwrap(),
+            proof_path.to_str().unwrap(),
+            public_input_path.to_str().unwrap(),
         )
         .unwrap();
     }
--- polling_service/parser/src/main.rs
+++ polling_service/parser/src/main.rs
@@ -5,8 +5,17 @@ use parser::parse_public_input;
 pub fn main() -> Result<(), String> {
     let args: Vec<String> = std::env::args().collect();
     let rpc_url = args.get(1).ok_or("Error: No RPC URL provided")?;
+    let output_path = args.get(2).ok_or("Error: No output path provided")?;
 
-    let mut state_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    state_path.push("../../protocol_state.pub");
-    parse_public_input(&rpc_url, state_path.to_str().unwrap())
+    let mut proof_path_buf = PathBuf::from(output_path);
+    proof_path_buf.push("protocol_state.proof");
+
+    let mut public_input_path_buf = PathBuf::from(output_path);
+    public_input_path_buf.push("protocol_state.pub");
+
+    parse_public_input(
+        &rpc_url,
+        proof_path_buf.to_str().unwrap(),
+        public_input_path_buf.to_str().unwrap(),
+    )
 }
