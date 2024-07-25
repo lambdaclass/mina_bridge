72b32b43a991a8b28b72975da93edce1eba5f299
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-07-25 11:32:37 -0300

    Added tokio and called submit proof function

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,8 +1,9 @@
 use std::path::PathBuf;
 
-use core::mina_polling_service;
+use core::{aligned_polling_service, mina_polling_service};
 
-pub fn main() -> Result<(), String> {
+#[tokio::main]
+async fn main() -> Result<(), String> {
     let args: Vec<String> = std::env::args().collect();
     let rpc_url = args.get(1).ok_or("Error: No RPC URL provided")?;
     let output_path = args.get(2).ok_or("Error: No output path provided")?;
@@ -13,13 +14,13 @@ pub fn main() -> Result<(), String> {
     let mut public_input_path_buf = PathBuf::from(output_path);
     public_input_path_buf.push("protocol_state.pub");
 
-    mina_polling_service::query_and_serialize(
+    let mina_proof = mina_polling_service::query_and_serialize(
         rpc_url,
         proof_path_buf.to_str().unwrap(),
         public_input_path_buf.to_str().unwrap(),
     )?;
 
-    // aligned_polling_service::submit_proof()
+    let _verification_data = aligned_polling_service::submit(&mina_proof).await?;
 
     Ok(())
 }
