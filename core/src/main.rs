e5f34026850e47c173f0357dff5de62901e4a17c
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-07-25 13:14:05 -0300

    Better logging

--- core/src/main.rs
+++ core/src/main.rs
@@ -2,7 +2,7 @@ extern crate dotenv;
 
 use core::{aligned_polling_service, mina_polling_service};
 use dotenv::dotenv;
-use log::info;
+use log::{error, info};
 use std::path::PathBuf;
 
 #[tokio::main]
@@ -21,15 +21,19 @@ async fn main() -> Result<(), String> {
     let mut public_input_path_buf = PathBuf::from(output_path);
     public_input_path_buf.push("protocol_state.pub");
 
+    // TODO(xqft): add logging to mina_polling_service
     info!("Executing Mina polling service");
     let mina_proof = mina_polling_service::query_and_serialize(
         &rpc_url,
         proof_path_buf.to_str().unwrap(),
         public_input_path_buf.to_str().unwrap(),
-    )?;
+    )
+    .inspect_err(|err| error!("{}", err))?;
 
     info!("Executing Aligned polling service");
-    let _verification_data = aligned_polling_service::submit(&mina_proof).await?;
+    let _verification_data = aligned_polling_service::submit(&mina_proof)
+        .await
+        .inspect_err(|err| error!("{}", err))?;
 
     Ok(())
 }
