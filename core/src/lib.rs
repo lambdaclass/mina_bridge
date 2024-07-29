db6d01fee50c9e3cf4783efb92afafc925b2aebc
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-07-29 12:44:23 -0300

    Fix module doc

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -2,8 +2,8 @@
 /// format.
 pub mod mina_polling_service;
 
-/// Service that sends a Mina proof to Aligned, awaits a response and updates the Bridge's smart
-/// contract.
+/// Service that sends a Mina proof to Aligned and awaits a response. Returns the verification
+/// data.
 pub mod aligned_polling_service;
 
 /// Utility for updating the bridge's smart contract with a new, verified state.
