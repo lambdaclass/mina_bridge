15aff805960c724dfe22fbb2f6548d30b8bbec6d
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-07-29 16:23:50 -0300

    Fix test

--- core/src/mina_polling_service.rs
+++ core/src/mina_polling_service.rs
@@ -164,10 +164,12 @@ fn serialize_protocol_state_proof(response_value: &serde_json::Value) -> Result<
 
 #[cfg(test)]
 mod tests {
+    use crate::utils::constants::ANVIL_PROOF_GENERATOR_ADDR;
+
     use super::query_and_serialize;
 
     #[test]
     fn serialize_and_deserialize() {
-        query_and_serialize("http://5.9.57.89:3085/graphql").unwrap();
+        query_and_serialize("http://5.9.57.89:3085/graphql", ANVIL_PROOF_GENERATOR_ADDR).unwrap();
     }
 }
