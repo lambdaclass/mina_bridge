b4de40a84ef9dee5d1f0212b1a16a20080b60592
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-08-08 17:50:40 -0300

    Base script

--- account_inclusion/sp1_prover/script/src/bin/main.rs
+++ account_inclusion/sp1_prover/script/src/bin/main.rs
@@ -1,79 +1,58 @@
-use alloy_sol_types::SolType;
-use clap::Parser;
+use core::{smart_contract_utility::get_tip_state_hash, utils::env::EnvironmentVariables};
+
+use account_inclusion_verifier::{query_leaf_and_merkle_path, query_merkle_root};
 use fibonacci_lib::PublicValuesStruct;
+use o1_utils::FieldHelpers;
 use sp1_sdk::{ProverClient, SP1Stdin};
 
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
-pub const FIBONACCI_ELF: &[u8] = include_bytes!("../../../elf/riscv32im-succinct-zkvm-elf");
+pub const VERIFICATION_ELF: &[u8] = include_bytes!("../../../elf/riscv32im-succinct-zkvm-elf");
 
-/// The arguments for the command.
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    #[clap(long)]
-    execute: bool,
-
-    #[clap(long)]
-    prove: bool,
-
-    #[clap(long, default_value = "20")]
-    n: u32,
-}
-
-fn main() {
+#[tokio::main]
+async fn main() {
     // Setup the logger.
     sp1_sdk::utils::setup_logger();
 
-    // Parse the command line arguments.
-    let args = Args::parse();
+    let EnvironmentVariables {
+        rpc_url,
+        chain,
+        eth_rpc_url,
+        ..
+    } = EnvironmentVariables::new().unwrap();
 
-    if args.execute == args.prove {
-        eprintln!("Error: You must specify either --execute or --prove");
-        std::process::exit(1);
-    }
+    let args: Vec<String> = std::env::args().collect();
+    let public_key = args.get(1).unwrap();
+
+    let state_hash = get_tip_state_hash(&chain, &eth_rpc_url).await.unwrap();
+
+    let (leaf_hash, merkle_path) = query_leaf_and_merkle_path(&rpc_url, public_key).unwrap();
+
+    let merkle_root = query_merkle_root(&rpc_url, state_hash).unwrap();
 
     // Setup the prover client.
     let client = ProverClient::new();
 
     // Setup the inputs.
     let mut stdin = SP1Stdin::new();
-    stdin.write(&args.n);
 
-    println!("n: {}", args.n);
-
-    if args.execute {
-        // Execute the program
-        let (output, report) = client.execute(FIBONACCI_ELF, stdin).run().unwrap();
-        println!("Program executed successfully.");
-
-        // Read the output.
-        let decoded = PublicValuesStruct::abi_decode(output.as_slice(), true).unwrap();
-        let PublicValuesStruct { n, a, b } = decoded;
-        println!("n: {}", n);
-        println!("a: {}", a);
-        println!("b: {}", b);
-
-        let (expected_a, expected_b) = fibonacci_lib::fibonacci(n);
-        assert_eq!(a, expected_a);
-        assert_eq!(b, expected_b);
-        println!("Values are correct!");
-
-        // Record the number of cycles executed.
-        println!("Number of cycles: {}", report.total_instruction_count());
-    } else {
-        // Setup the program for proving.
-        let (pk, vk) = client.setup(FIBONACCI_ELF);
-
-        // Generate the proof
-        let proof = client
-            .prove(&pk, stdin)
-            .run()
-            .expect("failed to generate proof");
-
-        println!("Successfully generated proof!");
-
-        // Verify the proof.
-        client.verify(&proof, &vk).expect("failed to verify proof");
-        println!("Successfully verified proof!");
+    stdin.write_vec(leaf_hash.to_bytes());
+    stdin.write_vec(merkle_root.to_bytes());
+    for merkle_node in merkle_path.into_iter() {
+        stdin.write_vec(merkle_node.to_bytes());
     }
+
+    // Setup the program for proving.
+    let (pk, vk) = client.setup(VERIFICATION_ELF);
+
+    // Generate the proof
+    let proof = client
+        .prove(&pk, stdin)
+        .run()
+        .expect("failed to generate proof");
+
+    println!("Successfully generated proof!");
+
+    // Verify the proof.
+    client.verify(&proof, &vk).expect("failed to verify proof");
+    println!("Successfully verified proof!");
 }
