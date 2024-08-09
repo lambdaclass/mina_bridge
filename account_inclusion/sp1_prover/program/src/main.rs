b26b5db17c165360f29c6a6f9c2cb66b3c9c8f38
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-08-09 11:09:35 -0300

    Nit

--- account_inclusion/sp1_prover/program/src/main.rs
+++ account_inclusion/sp1_prover/program/src/main.rs
@@ -1,10 +1,3 @@
-//! A simple program that takes a number `n` as input, and writes the `n-1`th and `n`th fibonacci
-//! number as an output.
-
-// These two lines are necessary for the program to properly compile.
-//
-// Under the hood, we wrap your main function with some extra code so that it behaves properly
-// inside the zkVM.
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
@@ -14,9 +7,11 @@ use kimchi::{mina_curves::pasta::Fp, o1_utils::FieldHelpers};
 pub fn main() {
     let leaf_hash_bytes = sp1_zkvm::io::read_vec();
     sp1_zkvm::io::commit_slice(&leaf_hash_bytes);
+    let leaf_hash = Fp::from_bytes(&leaf_hash_bytes).unwrap();
 
     let merkle_root_bytes = sp1_zkvm::io::read_vec();
     sp1_zkvm::io::commit_slice(&merkle_root_bytes);
+    let merkle_root = Fp::from_bytes(&merkle_root_bytes).unwrap();
 
     let merkle_tree_depth: usize = sp1_zkvm::io::read();
     sp1_zkvm::io::commit(&merkle_tree_depth);
@@ -27,9 +22,6 @@ pub fn main() {
         sp1_zkvm::io::commit_slice(&bytes);
         merkle_path_bytes.push(bytes);
     }
-
-    let leaf_hash = Fp::from_bytes(&leaf_hash_bytes).unwrap();
-    let merkle_root = Fp::from_bytes(&merkle_root_bytes).unwrap();
     let merkle_path: Vec<_> = merkle_path_bytes
         .into_iter()
         .map(|node_bytes| MerkleNode::from_bytes(node_bytes).unwrap())
