3be61b1384f4bf94e76bbcb0861bbf4850a7a470
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-09-09 12:55:44 -0300

    Add Mina account solidity struct

--- contract/src/lib/MinaAccountValidation.sol
+++ contract/src/lib/MinaAccountValidation.sol
@@ -1,24 +1,99 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.12;
 
-library MinaAccountValidation {
+contract MinaAccountValidation {
     struct Account {
-        CompressedPubKey publicKey;
+        CompressedECPoint publicKey;
         bytes32 tokenIdKeyHash;
         string tokenSymbol;
         uint64 balance;
         uint32 nonce;
         bytes32 receiptChainHash;
-        // delegate
-        // votingFor
-        // timing
-        // permissions
-        // zkapp
+        CompressedECPoint delegate;
+        bytes32 votingFor;
+        Timing timing;
+        Permissions permissions;
+        ZkappAccount zkapp;
     }
 
-    /// @notice A compressed public key (ec. point) of a Mina account.
-    struct CompressedPubKey {
+    /// @notice A compressed elliptic curve.
+    struct CompressedECPoint {
         bytes32 x;
         bool isOdd;
+    }
+
+    struct Timing {
+        uint64 initialMinimumBalance;
+        uint32 cliffTime;
+        uint64 cliffAmount;
+        uint32 vestingPeriod;
+        uint64 vestingIncrement;
+    }
+
+    enum AuthRequired {
+        None,
+        Either,
+        Proof,
+        Signature,
+        Impossible
+    }
+
+    struct Permissions {
+        AuthRequired editState;
+        AuthRequired access;
+        AuthRequired send;
+        AuthRequired rreceive;
+        AuthRequired setDelegate;
+        AuthRequired setPermissions;
+        AuthRequired setVerificationKeyAuth;
+        uint32 setVerificationKeyUint;
+        AuthRequired setZkappUri;
+        AuthRequired editActionState;
+        AuthRequired setTokenSymbol;
+        AuthRequired incrementNonce;
+        AuthRequired setVotingFor;
+        AuthRequired setTiming;
+    }
+
+    struct ZkappAccount {
+        bytes32[8] appState;
+        VerificationKey verificationKey;
+        uint32 zkappVersion;
+        bytes32[5] actionState;
+        uint32 lastActionSlot;
+        bool provedState;
+        bytes zkappUri;
+    }
+
+    struct VerificationKey {
+        ProofsVerified maxProofsVerified;
+        ProofsVerified actualWrapDomainSize;
+        WrapIndex wrapIndex;
+    }
+
+    enum ProofsVerified {
+        N0,
+        N1,
+        N2
+    }
+
+    struct WrapIndex {
+        Commitment[7] sigmaComm;
+        Commitment[15] coefficientsComm;
+        Commitment genericComm;
+        Commitment psmComm;
+        Commitment completeAddComm;
+        Commitment mulComm;
+        Commitment emulComm;
+        Commitment endomulScalarComm;
+    }
+
+    struct Commitment {
+        bytes32 x;
+        bytes32 y;
+    }
+
+    function validateAccount(Account calldata account) public returns (bool) {
+        return false;
     }
 }
