0100e5cf89762a77d67dfe8b3dbdad8938c99594
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-09-16 18:49:22 -0300

    Contract mockup

--- app/eth_contract/script/Sudoku.s.sol
+++ app/eth_contract/script/Sudoku.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {Sudoku} from "../src/Sudoku.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract SudokuScript is Script {
+    Sudoku public sudoku;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        sudoku = new Sudoku();
 
         vm.stopBroadcast();
     }
--- app/eth_contract/src/Sudoku.sol
+++ app/eth_contract/src/Sudoku.sol
@@ -1,11 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "mina_bridge/contract/src/MinaBridge.sol";
+import "mina_bridge/contract/src/MinaAccountValidation.sol";
+
 contract Sudoku {
     /// @notice The Sudoku zkApp verification key hash.
     uint256 public constant ZKAPP_VERIFICATION_KEY_HASH =
         19387792026269240922986233885372582803610254872042773421723960761233199555267;
 
-    /// @notice Validates a Sudoku solution by bridging from Mina
-    function validateSolution() external {}
+    /// @notice Mina bridge contract that validates and stores Mina states.
+    MinaBridge stateSettlement;
+
+    /// @notice Mina bridge contract that validates accounts
+    MinaAccountValidation accountValidation;
+
+    uint64 latestSolutionValidationAt = 0;
+
+    /// @notice Validates a Sudoku solution by bridging from Mina, and stores
+    /// the last Unix time it was solved at.
+    function validateSolution() external {
+        // 1. take the zkApp account of Sudoku.
+        // 2. take a relatively finalized state's ledger hash from
+        //    the stateSettlement contract.
+        // 3. verify the account for ledger hash (this involves calling the bridge core)
+        // 4. if the account is valid, extract the zkApp state it
+        // 5. if the isSolved bool is true, latestSolutionValidationAt = block.timestamp
+    }
 }
