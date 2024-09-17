b54823595e024679899c30046f3d7509abe44ddd
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-09-17 17:56:06 -0300

    zkApp script

--- example/mina_contract/src/run.ts
+++ example/mina_contract/src/run.ts
@@ -9,67 +9,108 @@
  * Build the project: `$ npm run build`
  * Run with node:     `$ node build/src/run.js`.
  */
+import fs from 'fs/promises';
 import { Sudoku, SudokuZkApp } from './sudoku.js';
 import { cloneSudoku, generateSudoku, solveSudoku } from './sudoku-lib.js';
-import { AccountUpdate, Mina, PrivateKey } from 'o1js';
+import { AccountUpdate, Mina, PrivateKey, NetworkId } from 'o1js';
 
-// setup
-const Devnet = await Mina.Network("https://api.minascan.io/node/devnet/v1/graphql");
-Mina.setActiveInstance(Devnet);
+const deployAlias = "devnet";
 
-const sender = Local.testAccounts[0];
-const senderKey = sender.key;
-const sudoku = generateSudoku(0.5);
-const zkAppPrivateKey = PrivateKey.random();
-const zkAppAddress = zkAppPrivateKey.toPublicKey();
-// create an instance of the smart contract
-const zkApp = new SudokuZkApp(zkAppAddress);
+// parse config and private key from file
+type Config = {
+  deployAliases: Record<
+    string,
+    {
+      networkId?: string;
+      url: string;
+      keyPath: string;
+      fee: string;
+      feepayerKeyPath: string;
+      feepayerAlias: string;
+    }
+  >;
+};
+let configJson: Config = JSON.parse(await fs.readFile('config.json', 'utf8'));
+let config = configJson.deployAliases[deployAlias];
+let feepayerKeysBase58: { privateKey: string; publicKey: string } = JSON.parse(
+  await fs.readFile(config.feepayerKeyPath, 'utf8')
+);
+let zkAppKeysBase58: { privateKey: string; publicKey: string } = JSON.parse(
+  await fs.readFile(config.keyPath, 'utf8')
+);
+let feepayerKey = PrivateKey.fromBase58(feepayerKeysBase58.privateKey);
+let zkAppKey = PrivateKey.fromBase58(zkAppKeysBase58.privateKey);
+let feepayerAddress = feepayerKey.toPublicKey();
+let zkAppAddress = zkAppKey.toPublicKey();
 
-console.log('Deploying and initializing Sudoku...');
-await SudokuZkApp.compile();
-let tx = await Mina.transaction(sender, async () => {
-  AccountUpdate.fundNewAccount(sender);
-  await zkApp.deploy();
-  await zkApp.update(Sudoku.from(sudoku));
+// define network (devnet)
+const Network = Mina.Network({
+  // We need to default to the testnet networkId if none is specified for this deploy alias in config.json
+  // This is to ensure the backward compatibility.
+  networkId: config.networkId as NetworkId,
+  mina: config.url,
 });
-await tx.prove();
-/**
- * note: this tx needs to be signed with `tx.sign()`, because `deploy` uses `requireSignature()` under the hood,
- * so one of the account updates in this tx has to be authorized with a signature (vs proof).
- * this is necessary for the deploy tx because the initial permissions for all account fields are "signature".
- * (but `deploy()` changes some of those permissions to "proof" and adds the verification key that enables proofs.
- * that's why we don't need `tx.sign()` for the later transactions.)
- */
-await tx.sign([zkAppPrivateKey, senderKey]).send();
+const fee = Number(config.fee) * 1e9; // in nanomina (1 billion = 1.0 mina)
+Mina.setActiveInstance(Network);
 
-console.log('Is the sudoku solved?', zkApp.isSolved.get().toBoolean());
+// define zkapp and create sudoku to upload
+const zkApp = new SudokuZkApp(zkAppAddress);
+const sudoku = generateSudoku(0.5);
+
+console.log('compiling Sudoku...');
+await SudokuZkApp.compile();
+
+console.log('updating sudoku to solve');
+try {
+  // call update() and send transaction
+  console.log('build transaction and create proof...');
+  let tx = await Mina.transaction(
+    { sender: feepayerAddress, fee },
+    async () => {
+      await zkApp.update(Sudoku.from(sudoku));
+    }
+  );
+  await tx.prove();
+
+  console.log('send transaction...');
+  const sentTx = await tx.sign([feepayerKey]).send();
+  if (sentTx.status === 'pending') {
+    console.log(
+      '\nSuccess! Update transaction sent.\n' +
+      '\nYour smart contract state will be updated' +
+      '\nas soon as the transaction is included in a block:' +
+      `\n${getTxnUrl(config.url, sentTx.hash)}`
+    );
+  }
+} catch (err) {
+  console.log(err);
+}
+
+console.log('Is the sudoku solved? (should be false)', zkApp.isSolved.get().toBoolean());
 
 let solution = solveSudoku(sudoku);
 if (solution === undefined) throw Error('cannot happen');
 
-// submit a wrong solution
-let noSolution = cloneSudoku(solution);
-noSolution[0][0] = (noSolution[0][0] % 9) + 1;
-
-console.log('Submitting wrong solution...');
-try {
-  let tx = await Mina.transaction(sender, async () => {
-    await zkApp.submitSolution(Sudoku.from(sudoku), Sudoku.from(noSolution));
-  });
-  await tx.prove();
-  await tx.sign([senderKey]).send();
-} catch {
-  console.log('There was an error submitting the solution, as expected');
-}
-
-console.log('Is the sudoku solved?', zkApp.isSolved.get().toBoolean());
-
-// submit the actual solution
+// submit the solution
 console.log('Submitting solution...');
-tx = await Mina.transaction(sender, async () => {
+let tx = await Mina.transaction({ sender: feepayerAddress, fee }, async () => {
   await zkApp.submitSolution(Sudoku.from(sudoku), Sudoku.from(solution!));
 });
 await tx.prove();
-await tx.sign([senderKey]).send();
+await tx.sign([feepayerKey]).send();
 
 console.log('Is the sudoku solved?', zkApp.isSolved.get().toBoolean());
+
+function getTxnUrl(graphQlUrl: string, txnHash: string | undefined) {
+  const hostName = new URL(graphQlUrl).hostname;
+  const txnBroadcastServiceName = hostName
+    .split('.')
+    .filter((item) => item === 'minascan')?.[0];
+  const networkName = graphQlUrl
+    .split('/')
+    .filter((item) => item === 'mainnet' || item === 'devnet')?.[0];
+  if (txnBroadcastServiceName && networkName) {
+    return `https://minascan.io/${networkName}/tx/${txnHash}?type=zk-tx`;
+  }
+  return `Transaction hash: ${txnHash}`;
+}
