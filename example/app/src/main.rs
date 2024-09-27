2b9c33147b8c5858bfe059c3ec9911968faba194
gabrielbosio <gabrielbosio95@gmail.com>
2024-09-27 16:55:46 -0300

    Update example address and unwrap bridge addresses

--- example/app/src/main.rs
+++ example/app/src/main.rs
@@ -11,15 +11,12 @@ use mina_bridge_core::{
         get_bridged_chain_tip_state_hash, update_bridge_chain, validate_account,
         AccountVerificationData,
     },
-    utils::{
-        env::EnvironmentVariables,
-        wallet, wallet_alloy,
-    },
+    utils::{env::EnvironmentVariables, wallet, wallet_alloy},
 };
 use std::{process, str::FromStr, time::SystemTime};
 
 const MINA_ZKAPP_ADDRESS: &str = "B62qmpq1JBejZYDQrZwASPRM5oLXW346WoXgbApVf5HJZXMWFPWFPuA";
-const SUDOKU_VALIDITY_DEVNET_ADDRESS: &str = "0xb19b36b1456E65E3A6D514D3F715f204BD59f431";
+const SUDOKU_VALIDITY_DEVNET_ADDRESS: &str = "0x8ce361602B935680E8DeC218b820ff5056BeB7af";
 const SUDOKU_VALIDITY_HOLESKY_ADDRESS: &str = "0x0091D1d9Bd92FFcfEb38383079AE849639224e3D";
 
 sol!(
@@ -61,6 +58,15 @@ async fn main() {
         private_key,
     } = EnvironmentVariables::new().unwrap_or_else(|err| {
         error!("{}", err);
+        process::exit(1);
+    });
+
+    let state_settlement_addr = state_settlement_addr.unwrap_or_else(|| {
+        error!("Error getting State settlement contract address");
+        process::exit(1);
+    });
+    let account_validation_addr = account_validation_addr.unwrap_or_else(|| {
+        error!("Error getting Account validation contract address");
         process::exit(1);
     });
 
@@ -125,13 +131,14 @@ async fn main() {
             let state_verification_result = update_bridge_chain(
                 &rpc_url,
                 &chain,
+                &state_settlement_addr,
                 &batcher_addr,
                 &batcher_eth_addr,
                 &eth_rpc_url,
                 &proof_generator_addr,
                 wallet.clone(),
                 &batcher_eth_addr,
-                false,
+                true,
                 false,
             )
             .await;
@@ -148,12 +155,13 @@ async fn main() {
             }
             // }
 
-            let tip_state_hash = get_bridged_chain_tip_state_hash(&state_settlement_addr, &eth_rpc_url)
-                .await
-                .unwrap_or_else(|err| {
-                    error!("{}", err);
-                    process::exit(1);
-                });
+            let tip_state_hash =
+                get_bridged_chain_tip_state_hash(&state_settlement_addr, &eth_rpc_url)
+                    .await
+                    .unwrap_or_else(|err| {
+                        error!("{}", err);
+                        process::exit(1);
+                    });
 
             let AccountVerificationData {
                 proof_commitment,
