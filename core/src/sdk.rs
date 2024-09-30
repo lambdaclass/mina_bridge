1f14fc32def2d60327d4fcf41e0d1f4c8ed59342
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-09-30 15:46:16 -0300

    Fix

--- core/src/sdk.rs
+++ core/src/sdk.rs
@@ -57,9 +57,10 @@ pub async fn update_bridge_chain(
 ) -> Result<(), String> {
     let (proof, pub_input) = get_mina_proof_of_state(rpc_url, chain, eth_rpc_url).await?;
 
-    let candidate_root_state_hash = pub_input.candidate_chain_state_hashes.first().unwrap();
-    if is_state_verified(&candidate_root_state_hash.to_string(), chain, eth_rpc_url).await? {
-        debug!("Candidate root {candidate_root_state_hash} is verified, so the bridge chain is updated");
+    if pub_input.candidate_chain_state_hashes
+        == get_bridge_chain_state_hashes(chain, eth_rpc_url).await?
+    {
+        debug!("The bridge chain is updated to the candidate chain");
         return Err("Latest chain is already verified".to_string());
     }
 
